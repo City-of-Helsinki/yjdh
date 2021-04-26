9365ac5dcd27d4a9b7f57071dd4c263e2dbf3cff
Kristian Klemets <kristian.klemets@anders.fi>
2021-04-26 15:22:32 +0300

    Add github workflow for backend

--- backend/kesaseteli/urls.py
+++ backend/kesaseteli/urls.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+#
+# Kubernetes liveness & readiness probes
+#
+def healthz(*args, **kwargs):
+    return HttpResponse(status=200)
+
+
+def readiness(*args, **kwargs):
+    return HttpResponse(status=200)
+
+
+urlpatterns += [path("healthz", healthz), path("readiness", readiness)]
