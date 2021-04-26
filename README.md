f4c5773bb8258f2fc6a9b37e82a1f4a5ab59186b
Kristian Klemets <kristian.klemets@anders.fi>
2021-04-26 13:57:26 +0300

    Add backend setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# kesaseteli
+YJDH-Kesäseteli
+
+## Development with Docker
+
+1. Copy the contents of .env.example to .env and modify it if needed.
+
+2. Run docker-compose up
+
+The backend is now running at [localhost:8000](http://localhost:8000)
---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+if [ -n "$DATABASE_HOST" ]; then
+  until nc -z -v -w30 "$DATABASE_HOST" 5432
+  do
+    echo "Waiting for postgres database connection..."
+    sleep 1
+  done
+  echo "Database is up!"
+fi
+
+# Apply database migrations
+if [[ "$APPLY_MIGRATIONS" = "1" ]]; then
+    echo "Applying database migrations..."
+    ./manage.py makemigrations
+    ./manage.py migrate --noinput
+fi
+
+# Create superuser
+if [[ "$CREATE_SUPERUSER" = "1" ]]; then
+    ./manage.py add_admin_user -u admin -p admin -e admin@example.com
+    echo "Admin user created with credentials admin:admin (email: admin@example.com)"
+fi
+
+# Start server
+if [[ ! -z "$@" ]]; then
+    "$@"
+elif [[ "$DEV_SERVER" = "1" ]]; then
+    python ./manage.py runserver 0.0.0.0:8000
+else
+    uwsgi --ini .prod/uwsgi.ini
+fi
