fc34d87464ec0f9d2607b7c1aa2f8c7894062c05
Kristian Klemets <kristian.klemets@anders.fi>
2021-04-27 10:27:56 +0300

    Reformat styles
    Refs YJDH-37

--- backend/kesaseteli/wsgi.py
+++ backend/kesaseteli/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "kesaseteli.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kesaseteli.settings")
 
 application = get_wsgi_application()
