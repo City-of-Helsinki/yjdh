bb74ce737093db6ec8e55c5966d7cf63a52220a6
Jaakko Nenonen <jaakko@codelicious.fi>
2021-05-21 18:29:52 +0300

    eslint fix

--- frontend/shared/browser-tests/utils/testcafe.utils.ts
+++ frontend/shared/browser-tests/utils/testcafe.utils.ts
@@ -14,7 +14,7 @@ export const setDataToPrintOnFailure = (
 const injectCallback = <
   Key extends string,
   Obj extends Record<Key, (...args) => Selector | SelectorPromise>
-  >(
+>(
   obj: Obj,
   callback: (...args) => void
 ): Obj =>
@@ -32,7 +32,6 @@ const injectCallback = <
       }),
       {}
     ) as Obj;
-
 
 export const screenContext = (t: TestController): typeof screen =>
   injectCallback(screen, (key: string, params) => {
@@ -63,7 +62,6 @@ export const withinContext = (t: TestController): typeof within => (selector) =>
     });
   });
 
-
 const getHtml = ClientFunction(
   // eslint-disable-next-line testing-library/no-node-access
   (selector: string) => document.querySelector(selector)?.outerHTML
@@ -72,19 +70,19 @@ const getHtml = ClientFunction(
 export const getErrorMessage = async (t: TestController): Promise<string> => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
   const testIdSelector = t.ctx.within?.key?.match(/testid/gi)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions
-    ? `[data-testid="${t.ctx.within.matcher}"]`
+    ? // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions
+      `[data-testid="${t.ctx.within.matcher}"]`
     : 'body';
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const componentHtml = pretty(await getHtml(testIdSelector)) as string;
-  return `Expectation failed on test context: 
+  const componentHtml = pretty((await getHtml(testIdSelector)) ?? '') ;
+  return `Expectation failed on test context:
   ------------------------------------------------
   ${JSON.stringify(t.ctx, null, '\t')}
   ------------------------------------------------
   Failure occured within '${testIdSelector}' component:
   ------------------------------------------------
   ${componentHtml}
-  ------------------------------------------------  
+  ------------------------------------------------
   `;
 };
 
