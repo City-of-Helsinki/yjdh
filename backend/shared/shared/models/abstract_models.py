b1207f210d7c6dc7610b7b9f1d2535905151f5a5
Khin Loc <khin.loc@anders.fi>
2021-06-03 14:15:01 +0300

    Add setup.py for shared component, update requirement.txt to install package locally from repo

---
+++ backend/shared/shared/models/abstract_models.py
@@ -0,0 +1,31 @@
+import uuid
+
+from django.db import models
+from simple_history.models import HistoricalRecords
+
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class HistoricalModel(models.Model):
+    """
+    Keeps a simple history of the changes for the model.
+    """
+
+    history = HistoricalRecords(inherit=True)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractCompany(models.Model):
+    """
+    TODO: Shared company fields go here
+    """
+
+    class Meta:
+        abstract = True
