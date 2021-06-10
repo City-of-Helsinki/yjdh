3279bbfe0d026deb449bac66ff913ed14c8e6f50
Kristian Klemets <kristian.klemets@anders.fi>
2021-06-10 15:33:59 +0300

    KS-Backend: Add HistoryRequestMiddleware
    Add the history middleware that was missing.
    
    Refs -

--- backend/kesaseteli/kesaseteli/settings.py
+++ backend/kesaseteli/kesaseteli/settings.py
@@ -120,6 +120,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_history.middleware.HistoryRequestMiddleware",
 ]
 
 TEMPLATES = [
