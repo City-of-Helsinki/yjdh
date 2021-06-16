2edc26f799b5cf7f995821098980370b5fc8424a
Jaakko Nenonen <jaakko@codelicious.fi>
2021-06-16 10:45:22 +0300

    fix build

--- frontend/shared/src/hocs/initLocale.tsx
+++ frontend/shared/src/hocs/initLocale.tsx
@@ -1,14 +1,10 @@
-import type { AppProps as NextJsAppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import { SSRConfig, useTranslation } from 'next-i18next';
+import { useTranslation } from 'next-i18next';
 import React from 'react';
 
-declare type AppProps = NextJsAppProps & {
-  pageProps: SSRConfig;
-};
-const initLocale = (
-  WrappedComponent: React.ComponentType<AppProps> | React.ElementType<AppProps>
-): typeof WrappedComponent => (props: AppProps) => {
+const initLocale = <P,>(
+  WrappedComponent: React.FC<P>
+): typeof WrappedComponent => (props: P) => {
   const router = useRouter();
   const { locale } = router;
   const { i18n } = useTranslation();
