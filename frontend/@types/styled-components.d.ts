64b000141534140aa3d6be29b0dae0b523773a76
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-06-24 09:49:35 +0300

    Add types for styled-components theme

---
+++ frontend/@types/styled-components.d.ts
@@ -0,0 +1,135 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      brick: string;
+      brickLight: string;
+      brickMediumLight: string;
+      brickDark: string;
+      bus: string;
+      busLight: string;
+      busMediumLight: string;
+      busDark: string;
+      coatOfArms: string;
+      coatOfArmsLight: string;
+      coatOfArmsMediumLight: string;
+      coatOfArmsDark: string;
+      copper: string;
+      copperLight: string;
+      copperMediumLight: string;
+      copperDark: string;
+      engel: string;
+      engelLight: string;
+      engelMediumLight: string;
+      engelDark: string;
+      fog: string;
+      fogLight: string;
+      fogMediumLight: string;
+      fogDark: string;
+      gold: string;
+      goldLight: string;
+      goldMediumLight: string;
+      goldDark: string;
+      metro: string;
+      metroLight: string;
+      metroMediumLight: string;
+      metroDark: string;
+      silver: string;
+      silverLight: string;
+      silverMediumLight: string;
+      silverDark: string;
+      summer: string;
+      summerLight: string;
+      summerMediumLight: string;
+      summerDark: string;
+      suomenlinna: string;
+      suomenlinnaLight: string;
+      suomenlinnaMediumLight: string;
+      suomenlinnaDark: string;
+      tram: string;
+      tramLight: string;
+      tramMediumLight: string;
+      tramDark: string;
+      black: string;
+      white: string;
+      black5: string;
+      black10: string;
+      black20: string;
+      black30: string;
+      black40: string;
+      black50: string;
+      black60: string;
+      black70: string;
+      black80: string;
+      black90: string;
+      error: string;
+      errorLight: string;
+      errorDark: string;
+      success: string;
+      successLight: string;
+      successDark: string;
+      alert: string;
+      alertLight: string;
+      alertDark: string;
+      info: string;
+      infoLight: string;
+      infoDark: string;
+    };
+    fonts: {
+      helGrotesk: string;
+    };
+    fontSize: {
+      heading: {
+        xl: string;
+        l: string;
+        m: string;
+        s: string;
+        xs: string;
+        xxs: string;
+      };
+      body: {
+        s: string;
+        m: string;
+        l: string;
+        xl: string;
+      };
+    };
+    containerWidth: {
+      xs: string;
+      s: string;
+      m: string;
+      l: string;
+      xl: string;
+    };
+    spacing: {
+      xs4: string;
+      xs3: string;
+      xs2: string;
+      xs: string;
+      s: string;
+      m: string;
+      l: string;
+      xl: string;
+      xl2: string;
+      xl3: string;
+      xl4: string;
+      xl5: string;
+    };
+    spacingLayout: {
+      xs2: string;
+      xs: string;
+      s: string;
+      m: string;
+      l: string;
+      xl: string;
+      xl2: string;
+    };
+    lineHeight: {
+      s: string;
+      m: string;
+      l: string;
+      xl: string;
+    };
+  }
+}
