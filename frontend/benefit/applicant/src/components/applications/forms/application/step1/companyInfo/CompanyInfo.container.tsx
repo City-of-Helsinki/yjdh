21bdf0f0e4c95cb6ea98f145a8bbf7f507e71e4a
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-06-30 19:27:26 +0300

    Extract company query to its own hook

--- frontend/benefit/applicant/src/components/applications/forms/application/step1/companyInfo/CompanyInfo.container.tsx
+++ frontend/benefit/applicant/src/components/applications/forms/application/step1/companyInfo/CompanyInfo.container.tsx
@@ -1,22 +1,13 @@
-import useBackendAPI from 'benefit/applicant/hooks/useBackendAPI';
-import { CompanyData } from 'benefit/applicant/types/company';
+import useCompanyQuery from 'benefit/applicant/hooks/useCompanyQuery';
 import React from 'react';
-import { useQuery } from 'react-query';
 
 import CompanyInfo, { CompanyInfoProps } from './CompanyInfo.component';
 
 export type CompanyInfoContainerProps = Omit<CompanyInfoProps, 'data'>;
 
 const CompanyInfoContainer: React.FC<CompanyInfoContainerProps> = (props) => {
-  const { axios, handleResponse } = useBackendAPI();
-  const { isLoading, error, data } = useQuery<CompanyData, { message: string }>(
-    'companyData',
-    async () => {
-      // TODO: replace the hardcoded company ID when auth is implemented
-      const res = axios.get<CompanyData>(`/v1/company/0877830-0`);
-      return handleResponse(res);
-    }
-  );
+  // TODO: replace the hardcoded company ID when auth is implemented
+  const { isLoading, error, data } = useCompanyQuery('0877830-0');
 
   const companyData = {
     name: data?.name ?? '',
