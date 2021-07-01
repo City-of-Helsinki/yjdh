8a566ca8c00cfb57a6816b42a756690573d807d9
Kristian Klemets <kristian.klemets@anders.fi>
2021-07-01 11:32:56 +0300

    KS-Backend: Add audit logging to applicaiton viewset
    - Adds audit logging to the applicaiton viewset CRUD events.
    - Add tests.
    
    Refs YJDH-149

--- backend/kesaseteli/applications/api/v1/views.py
+++ backend/kesaseteli/applications/api/v1/views.py
@@ -1,17 +1,16 @@
-from rest_framework import mixins, viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from shared.audit_log.viewsets import AuditLoggingModelViewSet
 
 from applications.api.v1.serializers import ApplicationSerializer
 from applications.models import Application
 
 
-class ApplicationViewSet(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    viewsets.GenericViewSet,
-):
+class ApplicationViewSet(AuditLoggingModelViewSet):
     queryset = Application.objects.select_related("company").prefetch_related(
         "summer_vouchers"
     )
     serializer_class = ApplicationSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
