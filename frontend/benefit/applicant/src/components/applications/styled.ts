3192912c1865622e742502b1a0505535e674754d
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-07-02 21:10:40 +0300

    Remove unused styles

--- frontend/benefit/applicant/src/components/applications/styled.ts
+++ frontend/benefit/applicant/src/components/applications/styled.ts
@@ -1,96 +1,5 @@
-import { APPLICATION_STATUSES } from 'benefit/applicant/constants';
 import { Button } from 'hds-react';
-import styled, { DefaultTheme } from 'styled-components';
-
-interface AvatarProps {
-  status: APPLICATION_STATUSES;
-}
-
-const avatarBackgroundColors = (
-  theme: DefaultTheme,
-  status?: APPLICATION_STATUSES
-): string => {
-  const colors = {
-    [APPLICATION_STATUSES.DRAFT]: theme.colors.black40,
-    [APPLICATION_STATUSES.INFO_REQUIRED]: theme.colors.alertDark,
-    [APPLICATION_STATUSES.RECEIVED]: theme.colors.info,
-    [APPLICATION_STATUSES.APPROVED]: theme.colors.success,
-    [APPLICATION_STATUSES.REJECTED]: theme.colors.error,
-  };
-  return colors[status || APPLICATION_STATUSES.DRAFT];
-};
-
-const StyledWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledHeading = styled.h1`
-  font-size: ${(props) => props.theme.fontSize.heading.m};
-  font-weight: 500;
-`;
-
-const StyledListWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledListItem = styled.div`
-  display: flex;
-  background-color: ${(props) => props.theme.colors.white};
-  padding: ${(props) => props.theme.spacing.xs};
-  justify-content: space-between;
-  margin-bottom: ${(props) => props.theme.spacing.xs2};
-`;
-
-const StyledItemContent = styled.div`
-  display: flex;
-`;
-
-const StyledAvatar = styled.div<AvatarProps>`
-  ${(props) => `
-    background-color: ${avatarBackgroundColors(props.theme, props.status)};
-    color: ${props.theme.colors.white};
-    font-size: ${props.theme.fontSize.heading.xs};
-  `}
-  font-weight: 600;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  height: 60px;
-  width: 60px;
-  min-height: 60px;
-  min-width: 60px;
-`;
-
-const StyledDataColumn = styled.div`
-  color: ${(props) => props.theme.colors.black90};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 0 ${(props) => props.theme.spacing.m};
-  &:nth-child(2) {
-    width: 140px;
-  }
-`;
-
-const StyledDataHeader = styled.div`
-  display: flex;
-  line-height: ${(props) => props.theme.lineHeight.xl};
-`;
-
-const StyledDataValue = styled.div`
-  display: flex;
-  line-height: ${(props) => props.theme.lineHeight.xl};
-  font-weight: 600;
-`;
-
-const StyledItemActions = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import styled from 'styled-components';
 
 const StyledSecondaryButton = styled(Button)`
   color: ${(props) => props.theme.colors.black90} !important;
@@ -149,21 +58,11 @@ const StyledEmpty = styled.div``;
 export {
   StyledApplicationAction,
   StyledApplicationActions,
-  StyledAvatar,
-  StyledDataColumn,
-  StyledDataHeader,
-  StyledDataValue,
   StyledEmpty,
   StyledHeaderItem,
-  StyledHeading,
-  StyledItemActions,
-  StyledItemContent,
-  StyledListItem,
-  StyledListWrapper,
   StyledPageHeader,
   StyledPageHeading,
   StyledPrimaryButton,
   StyledSecondaryButton,
   StyledSupplementaryButton,
-  StyledWrapper,
 };
