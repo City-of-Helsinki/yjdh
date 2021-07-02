b9cea9e777f22f74313c1bbfed6fc9e08f05dd8c
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-07-02 21:07:41 +0300

    Replace DraftList and SubmittedList with one component ApplicationList

---
+++ frontend/benefit/applicant/src/components/applications/applicationList/ApplicationList.tsx
@@ -0,0 +1,45 @@
+import { ApplicationData } from 'benefit/applicant/types/application';
+import React from 'react';
+import LoadingSkeleton from 'react-loading-skeleton';
+import Container from 'shared/components/container/Container';
+import theme from 'shared/styles/theme';
+
+import SC from './ApplicationList.sc';
+import ListItem from './listItem/ListItem';
+import useApplicationList from './useApplicationList';
+
+interface ApplicationListProps {
+  heading: string;
+  data: ApplicationData[];
+  isLoading: boolean;
+}
+
+const ApplicationsList: React.FC<ApplicationListProps> = ({
+  heading,
+  data,
+  isLoading,
+}) => {
+  const { list, shouldShowSkeleton, shouldHideList } = useApplicationList({
+    data,
+    isLoading,
+  });
+
+  const items = shouldShowSkeleton ? (
+    <ListItem isLoading />
+  ) : (
+    list?.map((props) => <ListItem key={props.id} {...props} />)
+  );
+
+  if (shouldHideList) return null;
+
+  return (
+    <Container backgroundColor={theme.colors.silverLight}>
+      <SC.Heading>
+        {shouldShowSkeleton ? <LoadingSkeleton width="20%" /> : heading}
+      </SC.Heading>
+      <SC.ListWrapper>{items}</SC.ListWrapper>
+    </Container>
+  );
+};
+
+export default ApplicationsList;
