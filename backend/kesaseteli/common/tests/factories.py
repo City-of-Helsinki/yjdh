4a2632ed10fbc4c2bf071055ff97893549900ca2
Kristian Klemets <kristian.klemets@anders.fi>
2021-08-18 10:11:29 +0300

    KS-Backend: Remove unused user factory
    Remove unused user factory and fix common user factory username
    so that it is saved properly.
    
    Refs -

--- backend/kesaseteli/common/tests/factories.py
+++ backend/kesaseteli/common/tests/factories.py
@@ -1,22 +1,10 @@
 import random
-import uuid
 
 import factory
-from django.contrib.auth import get_user_model
 
 from applications.enums import ApplicationStatus
 from applications.models import Application, SummerVoucher
 from companies.models import Company
-
-
-class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = get_user_model()
-
-    username = factory.Sequence(lambda n: "user_%s" % uuid.uuid4())
-    email = factory.Faker("email")
-    first_name = factory.Faker("first_name")
-    last_name = factory.Faker("last_name")
 
 
 class CompanyFactory(factory.django.DjangoModelFactory):
--- backend/shared/shared/common/tests/factories.py
+++ backend/shared/shared/common/tests/factories.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class UserFactory(factory.django.DjangoModelFactory):
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
-    username = factory.Sequence(lambda n: f"{factory.Faker('user_name')} {n}")
+    username = factory.Faker("user_name")
     email = factory.Faker("email")
 
     class Meta:
