7d91386d7f982e3e4063a3d508bfe8c46dd19007
Jaakko Nenonen <jaakko@codelicious.fi>
2021-08-20 18:07:34 +0300

    YJDH-62 basic wizard functionality (tests broken)

--- frontend/kesaseteli/employer/src/components/application/steps/step1/Step1Employer.sc.ts
+++
@@ -1,9 +0,0 @@
-import { $FormGroup } from 'shared/components/forms/section/FormSection.sc';
-import styled from 'styled-components';
-
-export const $EmployerBasicInfo = styled($FormGroup)`
-  display: grid;
-  grid-template-columns: 50% 50%;
-  grid-template-rows: 50% 50%;
-  grid-gap: ${(props) => props.theme.spacing.xs};
-`;
