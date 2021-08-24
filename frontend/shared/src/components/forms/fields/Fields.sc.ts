63b0bcb376dc3baaf39845833843447b871f8da7
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-08-24 17:44:59 +0300

    Replace styled components default import name

--- frontend/shared/src/components/forms/fields/Fields.sc.ts
+++ frontend/shared/src/components/forms/fields/Fields.sc.ts
@@ -1,7 +1,7 @@
 import { Checkbox, RadioButton, Select } from 'hds-react';
-import $ from 'styled-components';
+import styled from 'styled-components';
 
-export const $Checkbox = $(Checkbox)`
+export const $Checkbox = styled(Checkbox)`
   --background-selected: ${(props) => props.theme.colors.coatOfArms};
   --background-hover: ${(props) => props.theme.colors.coatOfArms};
   --border-color-selected: ${(props) => props.theme.colors.coatOfArms};
@@ -10,7 +10,7 @@ export const $Checkbox = $(Checkbox)`
   --focus-outline-color: ${(props) => props.theme.colors.coatOfArms};
 `;
 
-export const $RadioButton = $(RadioButton)`
+export const $RadioButton = styled(RadioButton)`
   --border-color-selected: ${(props) => props.theme.colors.coatOfArms};
   --border-color-selected-hover: ${(props) => props.theme.colors.coatOfArms};
   --border-color-selected-focus: ${(props) => props.theme.colors.coatOfArms};
@@ -18,7 +18,7 @@ export const $RadioButton = $(RadioButton)`
   --icon-color-hover: ${(props) => props.theme.colors.coatOfArms};
 `;
 
-export const $Dropdown = $(Select)`
+export const $Dropdown = styled(Select)`
   --border-color-selected: ${(props) => props.theme.colors.coatOfArms};
   --border-color-selected-hover: ${(props) => props.theme.colors.coatOfArms};
   --border-color-selected-focus: ${(props) => props.theme.colors.coatOfArms};
