815b150a45237022cb32a031a38d36e53ab3b8e4
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-08-24 17:37:47 +0300

    Replace babel config function with an object

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,12 +1,4 @@
-module.exports = function (api) {
-  api.cache(true);
-
-  const presets = ['next/babel', ['@babel/preset-typescript', { isTSX: true, allExtensions: true }]];
-
-  const plugins = [['styled-components', { ssr: true }]];
-
-  return {
-    presets,
-    plugins,
-  };
+module.exports = {
+  presets: ['next/babel', ['@babel/preset-typescript', { isTSX: true, allExtensions: true }]],
+  plugins: [['styled-components', { ssr: true }]],
 };
