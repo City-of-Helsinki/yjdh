db61ee8ee8d3953d3221f2b9b29e1db30b0fad9f
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-08-24 17:42:30 +0300

    Use HDS footer dark theme

--- frontend/benefit/applicant/src/components/footer/Footer.sc.ts
+++ frontend/benefit/applicant/src/components/footer/Footer.sc.ts
@@ -1,11 +1,5 @@
-import { Footer } from 'hds-react';
-import $ from 'styled-components';
+import styled from 'styled-components';
 
-export const $Footer = $(Footer)`
-  margin-top: ${(props) => props.theme.spacing.xl} !important;
-  --footer-background: ${(props) => props.theme.colors.black80} !important;
-  --footer-color: ${(props) => props.theme.colors.white} !important;
-  --footer-divider-color: ${(props) => props.theme.colors.white} !important;
-  --footer-focus-outline-color: ${(props) =>
-    props.theme.colors.white} !important;
+export const $FooterWrapper = styled.div`
+  margin-top: ${(props) => props.theme.spacing.xl4};
 `;
--- frontend/kesaseteli/employer/src/components/footer/Footer.tsx
+++ frontend/kesaseteli/employer/src/components/footer/Footer.tsx
@@ -1,17 +1,20 @@
+import { Footer } from 'hds-react';
 import { useTranslation } from 'next-i18next';
 import React from 'react';
 
-import { $Footer } from './Footer.sc';
+import { $FooterWrapper } from './Footer.sc';
 
 const FooterSection: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <$Footer title={t('common:appName')}>
-      <$Footer.Base
-        copyrightHolder={t('common:copyrightText')}
-        copyrightText={t('common:allRightsReservedText')}
-      />
-    </$Footer>
+    <$FooterWrapper>
+      <Footer title={t('common:appName')} theme="dark">
+        <Footer.Base
+          copyrightHolder={t('common:copyrightText')}
+          copyrightText={t('common:allRightsReservedText')}
+        />
+      </Footer>
+    </$FooterWrapper>
   );
 };
 
