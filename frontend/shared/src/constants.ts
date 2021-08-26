e0ddbd83ed4219d98d9703d0aba0e7c647d2f533
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-08-26 18:16:31 +0300

    Add a comment for REGEX constants

--- frontend/shared/src/constants.ts
+++ frontend/shared/src/constants.ts
@@ -1,6 +1,7 @@
 export const MAIN_CONTENT_ID = 'main_content';
 export const INVALID_FIELD_CLASS = 'invalid-field';
 
+// For the following regex constants, see: frontend/shared/src/__tests__/constants.test.ts
 export const ADDRESS_REGEX = /^([\d (),./A-Za-zÄÅÖäåö-]+)$/;
 export const COMPANY_BANK_ACCOUNT_NUMBER = /^FI\d{16}$/;
 export const PHONE_NUMBER_REGEX = /^((\+358[ -]*)+|(\\(\d{2,3}\\)[ -]*)|(\d{2,4})[ -]*)*?\d{3,4}?[ -]*\d{3,4}?$/;
