030e5e6667e41d7ea0b014835a7dbc169908b22a
Jaakko Nenonen <jaakko@codelicious.fi>
2021-08-31 10:39:29 +0300

    - ignore overlapping act() calls error as it's mot of the time useless message - README.md updated for instructions when developer gets too long branch name error

--- frontend/shared/src/__tests__/utils/setupTests.ts
+++ frontend/shared/src/__tests__/utils/setupTests.ts
@@ -3,7 +3,31 @@ import '@testing-library/jest-dom';
 import { toHaveNoViolations } from 'jest-axe';
 import JEST_TIMEOUT from 'shared/__tests__/utils/jest-timeout';
 
-
 jest.setTimeout(JEST_TIMEOUT);
 
 expect.extend(toHaveNoViolations);
+
+// hide initReactI18next warning
+// eslint-disable-next-line no-console
+const originalError = console.error;
+let consoleSpy: jest.SpyInstance;
+beforeAll(() => {
+  const messagesToIgnore = [
+    'Warning: You seem to have overlapping act() calls, this is not supported',
+    'react-i18next:: You will need to pass in an i18next instance by using initReactI18next',
+  ];
+
+  consoleSpy = jest.spyOn(console, 'warn').mockImplementation((...args) => {
+    if (
+      typeof args[0] === 'string' &&
+      messagesToIgnore.some((msg) => args[0].includes(msg))
+    ) {
+      return () => {};
+    }
+    return originalError.call(console, args);
+  });
+});
+
+afterAll(() => {
+  consoleSpy.mockRestore();
+});
