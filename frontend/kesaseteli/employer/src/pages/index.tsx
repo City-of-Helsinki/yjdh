608c49288161a245b791a65ec666d232de6dbb53
Jaakko Nenonen <jaakko@codelicious.fi>
2021-08-31 23:20:09 +0300

    still fixing merge conflicts

--- frontend/kesaseteli/employer/src/pages/index.tsx
+++ frontend/kesaseteli/employer/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import useLoadDraftOrCreateNewApplication from 'kesaseteli/employer/hooks/application/useLoadDraftOrCreateNewApplication';
 import useApplicationsQuery from 'kesaseteli/employer/hooks/backend/useApplicationsQuery';
 import useCreateApplicationQuery from 'kesaseteli/employer/hooks/backend/useCreateApplicationQuery';
 import useLogoutQuery from 'kesaseteli/employer/hooks/backend/useLogoutQuery';
@@ -8,51 +9,49 @@ import React from 'react';
 import withAuth from 'shared/components/hocs/withAuth';
 import ErrorPage from 'shared/components/pages/ErrorPage';
 import PageLoadingSpinner from 'shared/components/pages/PageLoadingSpinner';
+import useIsSyncingToBackend from 'shared/hooks/useIsSyncingToBackend';
 import getServerSideTranslations from 'shared/i18n/get-server-side-translations';
-import { DEFAULT_LANGUAGE } from 'shared/i18n/i18n';
 
 const EmployerIndex: NextPage = () => {
+  const { isSyncing, isMutating } = useIsSyncingToBackend();
+  const { mutate: logout, isLoading: isLoadingLogout } = useLogoutQuery();
+  const isLoading = isSyncing || isLoadingLogout;
+
   const {
     data: applications,
-    isLoading: isLoadingApplications,
-    error: loadApplicationsError,
-  } = useApplicationsQuery();
-  const { mutate: createApplication, error: createApplicationError, isLoading: isCreatingApplication } =
-    useCreateApplicationQuery();
-
+    isError: loadApplicationsError,
+  } = useApplicationsQuery(!isMutating);
   const {
-    mutate: logout,
-    isLoading: isLoadingLogout,
-  } = useLogoutQuery();
+    data: newApplication,
+    mutate: createApplication,
+    isError: createApplicationError,
+  } = useCreateApplicationQuery();
 
-  const isLoading = isLoadingApplications || isCreatingApplication || isLoadingLogout;
   const isError = loadApplicationsError || createApplicationError;
-  const router = useRouter();
-  const locale = router.locale ?? DEFAULT_LANGUAGE;
 
-  React.useEffect(() => {
-    if (!isLoading && !isError) {
-      if (applications && applications.length > 0) {
-        const draftApplication = applications[0];
-        void router.push(`${locale}/application?id=${draftApplication.id}`);
-      } else {
-        createApplication();
-      }
-    }
-  }, [isLoading, applications, createApplication, router, locale, isError]);
+  useLoadDraftOrCreateNewApplication(
+    isError,
+    applications,
+    newApplication,
+    createApplication
+  );
+
+  const router = useRouter();
+  const refreshPage = (): void => {
+    router.reload();
+  };
 
   const { t } = useTranslation();
-  if (isLoading) {
-    return (
-      <PageLoadingSpinner/>
-    );
+  if (isSyncing) {
+    return <PageLoadingSpinner />;
   }
   if (isError && !isLoading) {
     return (
       <ErrorPage
         title={t('common:errorPage.title')}
         message={t('common:errorPage.message')}
-        onLogout={logout as () => void}
+        logout={logout as () => void}
+        retry={refreshPage}
       />
     );
   }
@@ -60,7 +59,8 @@ const EmployerIndex: NextPage = () => {
   return <></>;
 };
 
-export const getStaticProps: GetStaticProps =
-  getServerSideTranslations('common');
+export const getStaticProps: GetStaticProps = getServerSideTranslations(
+  'common'
+);
 
 export default withAuth(EmployerIndex);
