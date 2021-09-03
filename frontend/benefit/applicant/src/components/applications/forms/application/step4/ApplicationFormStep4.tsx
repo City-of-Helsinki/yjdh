341fbd068186ab35ab54f890aaf4afc24f77046b
Ivan Melnik <melnik.job@gmail.com>
2021-09-03 11:46:29 +0300

    init

--- frontend/benefit/applicant/src/components/applications/forms/application/step4/ApplicationFormStep4.tsx
+++ frontend/benefit/applicant/src/components/applications/forms/application/step4/ApplicationFormStep4.tsx
@@ -1,16 +1,86 @@
+import { $SupplementaryButton } from 'benefit/applicant/components/applications/Applications.sc';
 import { useTranslation } from 'benefit/applicant/i18n';
+import { DynamicFormStepComponentProps } from 'benefit/applicant/types/common';
+import { IconPen } from 'hds-react';
 import * as React from 'react';
+import Heading from 'shared/components/forms/heading/Heading';
 import FormSection from 'shared/components/forms/section/FormSection';
 
-const ApplicationFormStep4: React.FC = () => {
-  const { t } = useTranslation();
-  const translationsBase = 'common:applications.sections.summary';
+import StepperActions from '../stepperActions/StepperActions';
+import { useApplicationFormStep4 } from './useApplicationFormStep4';
 
+const ApplicationFormStep4: React.FC<DynamicFormStepComponentProps> = ({
+  data,
+}) => {
+  const { t } = useTranslation();
+  const { handleBack, handleNext, handleStepChange } = useApplicationFormStep4(
+    data
+  );
+  const translationsBase = 'common:applications.sections';
+  // console.log(data);
   return (
     <>
-      <FormSection header={t(`${translationsBase}.heading1`)}>
-        Content
+      <FormSection
+        action={
+          <$SupplementaryButton
+            onClick={() => handleStepChange(1)}
+            variant="supplementary"
+            iconLeft={<IconPen />}
+          >
+            {t(`common:applications.actions.edit`)}
+          </$SupplementaryButton>
+        }
+      >
+        <Heading as="h2" header={t(`${translationsBase}.company.heading1`)} />
+        <Heading
+          as="h2"
+          header={t(`${translationsBase}.company.heading2Short`)}
+        />
+        <Heading as="h2" header={t(`${translationsBase}.company.heading3`)} />
       </FormSection>
+      <FormSection
+        action={
+          <$SupplementaryButton
+            onClick={() => handleStepChange(2)}
+            variant="supplementary"
+            iconLeft={<IconPen />}
+          >
+            {t(`common:applications.actions.edit`)}
+          </$SupplementaryButton>
+        }
+      >
+        <Heading
+          as="h2"
+          header={t(`${translationsBase}.employee.heading1Short`)}
+        />
+        <Heading as="h2" header={t(`${translationsBase}.employee.heading2`)} />
+        <Heading
+          as="h2"
+          header={t(`${translationsBase}.employee.heading3Long`)}
+        />
+      </FormSection>
+      <FormSection
+        action={
+          <$SupplementaryButton
+            onClick={() => handleStepChange(3)}
+            variant="supplementary"
+            iconLeft={<IconPen />}
+          >
+            {t(`common:applications.actions.edit`)}
+          </$SupplementaryButton>
+        }
+      >
+        <Heading
+          as="h2"
+          header={t(`${translationsBase}.attachments.heading1`)}
+        />
+      </FormSection>
+      <StepperActions
+        hasBack
+        hasNext
+        handleSubmit={handleNext}
+        handleBack={handleBack}
+      />
     </>
   );
 };
---
+++ frontend/benefit/applicant/src/components/applications/forms/application/step4/useApplicationFormStep4.ts
@@ -0,0 +1,82 @@
+import hdsToast from 'benefit/applicant/components/toast/Toast';
+import ApplicationContext from 'benefit/applicant/context/ApplicationContext';
+import useUpdateApplicationQuery from 'benefit/applicant/hooks/useUpdateApplicationQuery';
+import { useTranslation } from 'benefit/applicant/i18n';
+import {
+  Application,
+  ApplicationData,
+} from 'benefit/applicant/types/application';
+import { getApplicationStepString } from 'benefit/applicant/utils/common';
+import React, { useEffect, useState } from 'react';
+import snakecaseKeys from 'snakecase-keys';
+
+type ExtendedComponentProps = {
+  handleNext: () => void;
+  handleBack: () => void;
+  handleStepChange: (step: number) => void;
+};
+
+const useApplicationFormStep4 = (
+  application: Application
+): ExtendedComponentProps => {
+  const { applicationTempData, setApplicationTempData } = React.useContext(
+    ApplicationContext
+  );
+  const { t } = useTranslation();
+  const [step, setStep] = useState<number>(3);
+
+  const {
+    mutate: updateApplicationStep4,
+    error: updateApplicationErrorStep4,
+    isSuccess: isApplicationUpdatedStep4,
+  } = useUpdateApplicationQuery();
+
+  useEffect(() => {
+    // todo:custom error messages
+    if (updateApplicationErrorStep4) {
+      hdsToast({
+        autoDismiss: true,
+        autoDismissTime: 5000,
+        type: 'error',
+        translated: true,
+        labelText: t('common:error.generic.label'),
+        text: t('common:error.generic.text'),
+      });
+    }
+  }, [t, updateApplicationErrorStep4]);
+
+  useEffect(() => {
+    if (isApplicationUpdatedStep4) {
+      setApplicationTempData({ ...applicationTempData, currentStep: step });
+    }
+  }, [
+    isApplicationUpdatedStep4,
+    applicationTempData,
+    step,
+    setApplicationTempData,
+  ]);
+
+  const handleStepChange = (nextStep: number): void => {
+    setStep(nextStep);
+    const currentApplicationData: ApplicationData = snakecaseKeys(
+      {
+        ...application,
+        applicationStep: getApplicationStepString(nextStep),
+      },
+      { deep: true }
+    );
+    updateApplicationStep4(currentApplicationData);
+  };
+
+  const handleNext = (): void => handleStepChange(5);
+
+  const handleBack = (): void => handleStepChange(3);
+
+  return {
+    handleNext,
+    handleBack,
+    handleStepChange,
+  };
+};
+
+export { useApplicationFormStep4 };
