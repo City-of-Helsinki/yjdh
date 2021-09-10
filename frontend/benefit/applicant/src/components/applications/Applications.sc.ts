eea1ebf6490154e64c5fe689ac595746aff5d501
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-09-10 15:34:37 +0300

    Remove redundant styles

--- frontend/benefit/applicant/src/components/applications/Applications.sc.ts
+++ frontend/benefit/applicant/src/components/applications/Applications.sc.ts
@@ -62,17 +62,6 @@ export const $PageHeadingHelperText = styled.div`
   margin-bottom: ${(props) => props.theme.spacing.m};
 `;
 
-export const $ApplicationActions = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: ${(props) => props.theme.spacing.l};
-`;
-
-export const $ApplicationAction = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
 export const $SpinnerContainer = styled.div`
   display: flex;
   justify-content: center;
