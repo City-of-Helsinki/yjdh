dbf22d7bad7715193c79700e253edc0bb238fdc7
Khin Loc <khin.loc@anders.fi>
2021-09-15 17:41:38 +0300

    Remove snapshot tests

--- backend/benefit/applications/tests/test_ahjo_integration.py
+++ backend/benefit/applications/tests/test_ahjo_integration.py
@@ -3,7 +3,6 @@ import zipfile
 from datetime import date
 from unittest.mock import patch
 
-import factory.random
 import pytest
 from applications.enums import ApplicationStatus
 from applications.models import Application
@@ -16,9 +15,11 @@ from applications.tests.factories import ApplicationFactory
 from helsinkibenefit.tests.conftest import *  # noqa
 
 
-@pytest.fixture(autouse=True)
-def reseed_fixtures(db, django_db_setup, django_db_blocker):
-    factory.random.reseed_random("888")
+def _assert_html_content(html, include_keys=(), excluded_keys=()):
+    for k in include_keys:
+        assert k in html
+    for k in excluded_keys:
+        assert k not in html
 
 
 @pytest.mark.parametrize(
@@ -30,20 +31,22 @@ def reseed_fixtures(db, django_db_setup, django_db_blocker):
     ],
 )
 @patch("applications.services.ahjo_integration.pdfkit.from_string")
-def test_generate_single_template_html(
-    mock_pdf_convert, snapshot, company_type, de_minimis_aid
-):
+def test_generate_single_template_html(mock_pdf_convert, company_type, de_minimis_aid):
     mock_pdf_convert.return_value = {}
     app = ApplicationFactory(
         company__company_form=company_type, de_minimis_aid=de_minimis_aid
     )
-    # Only assert html snapshot for easier comparison
+    # Only assert html content for easier comparison
     _, _, html = generate_single_file(app)
-    snapshot.assert_match(html)
+
+    _assert_html_content(
+        html,
+        (app.ahjo_application_number, app.employee.first_name, app.employee.last_name),
+    )
 
 
 @patch("applications.services.ahjo_integration.pdfkit.from_string")
-def test_generate_composed_template_html(mock_pdf_convert, snapshot):
+def test_generate_composed_template_html(mock_pdf_convert):
     mock_pdf_convert.return_value = {}
     app_1 = ApplicationFactory.create(
         status=ApplicationStatus.ACCEPTED, start_date=date.today()
@@ -56,19 +59,34 @@ def test_generate_composed_template_html(mock_pdf_convert, snapshot):
     )
     apps = Application.objects.all()
 
-    # Only snapshot html content for easier comparison
+    # Only assert html content for easier comparison
     files = generate_composed_files(apps)
     assert len(files) == 4
-    snapshot.assert_match([f[0] for f in files])
 
-    assert app_1.ahjo_application_number in files[0][2]
-    assert app_1.ahjo_application_number not in files[3][2]
-
-    assert app_2.ahjo_application_number not in files[0][2]
-    assert app_2.ahjo_application_number in files[3][2]
-
-    assert app_3.ahjo_application_number not in files[0][2]
-    assert app_3.ahjo_application_number not in files[3][2]
+    # files[0]: Public accepted composed files
+    # files[1]: Private accepted composed files
+    # files[2]: Public rejected composed files
+    # files[3]: Private rejected composed files
+    _assert_html_content(
+        files[0][2],
+        (app_1.ahjo_application_number),
+        (app_2.ahjo_application_number, app_3.ahjo_application_number),
+    )
+    _assert_html_content(
+        files[1][2],
+        (app_1.ahjo_application_number),
+        (app_2.ahjo_application_number, app_3.ahjo_application_number),
+    )
+    _assert_html_content(
+        files[2][2],
+        (app_2.ahjo_application_number),
+        (app_1.ahjo_application_number, app_3.ahjo_application_number),
+    )
+    _assert_html_content(
+        files[3][2],
+        (app_2.ahjo_application_number),
+        (app_1.ahjo_application_number, app_3.ahjo_application_number),
+    )
 
 
 def test_export_application_batch(application_batch):
