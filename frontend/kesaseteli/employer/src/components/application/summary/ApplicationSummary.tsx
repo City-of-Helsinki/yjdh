09f5b371af0143f97b46cd802384a53f44c5fecd
Jaakko Nenonen <jaakko@codelicious.fi>
2021-09-21 21:09:36 +0300

    data-test-ids and fix formatting

--- frontend/kesaseteli/employer/src/components/application/summary/ApplicationSummary.tsx
+++ frontend/kesaseteli/employer/src/components/application/summary/ApplicationSummary.tsx
@@ -5,6 +5,7 @@ import FormSection from 'shared/components/forms/section/FormSection';
 import { $GridCell, $Hr } from 'shared/components/forms/section/FormSection.sc';
 import FormSectionHeading from 'shared/components/forms/section/FormSectionHeading';
 import PageLoadingSpinner from 'shared/components/pages/PageLoadingSpinner';
+import { convertToUIDateFormat } from 'shared/utils/date.utils';
 
 const ApplicationSummary: React.FC = () => {
   const { t } = useTranslation();
@@ -37,16 +38,19 @@ const ApplicationSummary: React.FC = () => {
           header={`${company.name}, ${company.business_id}`}
           size="s"
           as="div"
+          data-testid="company-heading"
         />
-        <$GridCell as="pre">
+        <$GridCell as="pre" data-testid="company-data">
           {company.industry}, {company.company_form}, {company.postcode}{' '}
           {company.city}
         </$GridCell>
-        <$GridCell as="pre">
+        <$GridCell as="pre" data-testid="contact-person">
           {contact_person_name}, {contact_person_email},{' '}
           {contact_person_phone_number}
         </$GridCell>
-        <$GridCell as="pre">{street_address}</$GridCell>
+        <$GridCell as="pre" data-testid="street-address">
+          {street_address}
+        </$GridCell>
         {/* TODO: Laskuttajan Lisäkentät */}
       </FormSection>
       <FormSection
@@ -55,7 +59,7 @@ const ApplicationSummary: React.FC = () => {
         columns={1}
         withoutDivider
       >
-        {summer_vouchers.map((employment) => (
+        {summer_vouchers.map((employment, index) => (
           <>
             <FormSectionHeading
               header={`${employment.employee_name ?? ''} ${
@@ -64,43 +68,52 @@ const ApplicationSummary: React.FC = () => {
               size="s"
               as="h3"
               key={employment.id}
+              data-testid={`employee-info-${index}`}
             />
-            <$GridCell as="pre">
+            <$GridCell
+              as="pre"
+              data-testid={`summer_voucher_exception_reason_${index}`}
+            >
               {t(
                 `common:application.form.selectionGroups.summer_voucher_exception_reason.${
                   employment.summer_voucher_exception_reason ?? ''
                 }`
               )}
             </$GridCell>
-            <$GridCell as="pre">
+            <$GridCell as="pre" data-testid={`employee_postcode_${index}`}>
               {t('common:application.form.inputs.employee_postcode')}:{' '}
               {employment.employee_postcode}
             </$GridCell>
-            <$GridCell as="pre">
+            <$GridCell as="pre" data-testid={`employee_home_city_${index}`}>
               {t('common:application.form.inputs.employee_home_city')}:{' '}
               {employment.employee_home_city}
             </$GridCell>
-            <$GridCell as="pre">
+            <$GridCell as="pre" data-testid={`employee_phone_number_${index}`}>
               {t('common:application.form.inputs.employee_phone_number')}:{' '}
-              {employment.employee_phone_number},{' '}
-              {employment.employment_postcode}
+              {employment.employee_phone_number}
             </$GridCell>
-            <$GridCell as="pre">
+            <$GridCell as="pre" data-testid={`employment_postcode_${index}`}>
               {t('common:application.form.inputs.employment_postcode')}:{' '}
               {employment.employment_postcode}
             </$GridCell>
-            <$GridCell as="pre">
+            <$GridCell
+              as="pre"
+              data-testid={`summer_voucher_serial_number_${index}`}
+            >
               {t('common:application.form.inputs.summer_voucher_serial_number')}
-              : {employment.summer_voucher_serial_number},{' '}
+              : {employment.summer_voucher_serial_number}
+            </$GridCell>
+            <$GridCell as="pre" data-testid={`employee_school_${index}`}>
+              {t('common:application.form.inputs.employee_school')}:{' '}
               {employment.employee_school}
             </$GridCell>
             {/* TODO: palkkatodistus, työsopimus attachments */}
-            <$GridCell as="pre">
+            <$GridCell as="pre" data-testid={`employment_${index}`}>
               {t('common:application.step2.employment')}:{' '}
-              {employment.employment_start_date}-
-              {employment.employment_end_date}
+              {convertToUIDateFormat(employment.employment_start_date)} -{' '}
+              {convertToUIDateFormat(employment.employment_end_date)}
             </$GridCell>
-            <$GridCell as="pre">
+            <$GridCell as="pre" data-testid={`employment_work_hours_${index}`}>
               {t('common:application.form.inputs.employment_work_hours')}:{' '}
               {employment.employment_work_hours},{' '}
               {t('common:application.form.inputs.employment_salary_paid')}:{' '}
@@ -111,12 +124,18 @@ const ApplicationSummary: React.FC = () => {
                 <$GridCell as="pre">
                   {t('common:application.form.inputs.employment_description')}:
                 </$GridCell>
-                <$GridCell as="pre">
+                <$GridCell
+                  as="pre"
+                  data-testid={`employment_description_${index}`}
+                >
                   {employment.employment_description}
                 </$GridCell>
               </>
             )}
-            <$GridCell as="pre">
+            <$GridCell
+              as="pre"
+              data-testid={`hired_without_voucher_assessment_${index}`}
+            >
               {t(
                 'common:application.form.inputs.hired_without_voucher_assessment'
               )}
