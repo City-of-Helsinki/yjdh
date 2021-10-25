757c67f47f01eb121e51b3eadb45278e77b19193
Jaakko Nenonen <jaakko@codelicious.fi>
2021-10-25 11:46:22 +0300

    run prettier

--- frontend/kesaseteli/employer/src/hooks/backend/useUserQuery.ts
+++ frontend/kesaseteli/employer/src/hooks/backend/useUserQuery.ts
@@ -2,7 +2,7 @@ import { BackendEndpoint } from 'kesaseteli/employer/backend-api/backend-api';
 import useLogoutQuery from 'kesaseteli/employer/hooks/backend/useLogoutQuery';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
-import { useQuery, UseQueryOptions,UseQueryResult } from 'react-query';
+import { useQuery, UseQueryOptions, UseQueryResult } from 'react-query';
 import handleError from 'shared/error-handler/error-handler';
 import useLocale from 'shared/hooks/useLocale';
 import User from 'shared/types/user';
