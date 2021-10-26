97ba2746564aa3e1c500e6dae9d5d8efc7bb4aae
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-10-26 07:01:27 +0300

    Custom error handling for the UserInfo query

--- frontend/benefit/applicant/src/hooks/useUserQuery.ts
+++ frontend/benefit/applicant/src/hooks/useUserQuery.ts
@@ -1,28 +1,41 @@
 import { BackendEndpoint } from 'benefit/applicant/backend-api/backend-api';
 import useBackendAPI from 'benefit/applicant/hooks/useBackendAPI';
-import useLogoutQuery from 'benefit/applicant/hooks/useLogoutQuery';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { useQuery, UseQueryResult } from 'react-query';
-import handleError from 'shared/error-handler/error-handler';
+import showErrorToast from 'shared/components/toast/show-error-toast';
 import useLocale from 'shared/hooks/useLocale';
 import User from 'shared/types/user';
 
 const useUserQuery = <T = User>(
   select?: (user: User) => T
 ): UseQueryResult<T, Error> => {
-  const logoutQuery = useLogoutQuery();
   const { t } = useTranslation();
   const router = useRouter();
+  const { logout } = router.query;
   const locale = useLocale();
   const { axios, handleResponse } = useBackendAPI();
+
+  const handleError = (error: Error): void => {
+    if (logout) {
+      void router.push(`${locale}/login?logout=true`);
+    } else if (/40[13]/.test(error.message)) {
+      void router.push(`${locale}/login`);
+    } else {
+      showErrorToast(
+        t('common:error.generic.label'),
+        t('common:error.generic.text')
+      );
+    }
+  };
 
   return useQuery(
     `${BackendEndpoint.USER}`,
     () => handleResponse<User>(axios.get(BackendEndpoint.USER)),
     {
-      enabled: !logoutQuery.isLoading,
-      onError: (error) => handleError(error, t, router, locale),
+      enabled: !logout,
+      onError: (error) => handleError(error),
+      retry: false,
       select,
     }
   );
