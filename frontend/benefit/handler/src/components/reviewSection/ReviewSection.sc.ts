c6ce32a87787c617ef74926a83e5dcdfe59753b6
Ivan Melnik <melnik.job@gmail.com>
2021-11-13 19:07:19 +0200

    init

---
+++ frontend/benefit/handler/src/components/reviewSection/ReviewSection.sc.ts
@@ -0,0 +1,34 @@
+import { IconCheckCircle } from 'hds-react';
+import styled from 'styled-components';
+
+export type ReviewSectionWrapperProps = {
+  withAction?: boolean;
+};
+
+export const $Wrapper = styled.div<ReviewSectionWrapperProps>`
+  background-color: ${(props) =>
+    props.withAction ? props.theme.colors.silverLight : ''};
+  display: ${(props) => (props.withAction ? 'flex' : '')};
+  flex-direction: ${(props) => (props.withAction ? 'column' : '')};
+`;
+
+export const $WrapperInner = styled.div<ReviewSectionWrapperProps>`
+  background-color: ${(props) =>
+    props.withAction ? props.theme.colors.silverLight : ''};
+  display: ${(props) => (props.withAction ? 'flex' : '')};
+`;
+
+export const $ActionLeft = styled.div`
+  width: 100px;
+`;
+
+export const $CheckIcon = styled(IconCheckCircle)`
+  padding-top: 20px;
+  padding-left: ${(props) => props.theme.spacing.s};
+`;
+
+export const $ActionBottom = styled.div`
+  background-color: ${(props) => props.theme.colors.silver};
+  padding: ${(props) => props.theme.spacing.s};
+  padding-left: 100px;
+`;
---
+++ frontend/benefit/handler/src/components/reviewSection/ReviewSection.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Heading from 'shared/components/forms/heading/Heading';
+import { HeadingProps } from 'shared/components/forms/heading/Heading.sc';
+import {
+  $Grid,
+  $Hr,
+  $Section,
+  GridProps,
+} from 'shared/components/forms/section/FormSection.sc';
+import { useTheme } from 'styled-components';
+
+import {
+  $ActionBottom,
+  $ActionLeft,
+  $CheckIcon,
+  $Wrapper,
+  $WrapperInner,
+} from './ReviewSection.sc';
+
+export type ReviewSectionProps = {
+  children?: React.ReactNode;
+  action?: React.ReactNode;
+  withoutDivider?: boolean;
+  header?: string;
+} & HeadingProps &
+  GridProps;
+
+const ReviewSection: React.FC<ReviewSectionProps> = ({
+  children,
+  header,
+  action,
+  role,
+  loading,
+  ...rest
+}) => {
+  const theme = useTheme();
+
+  return (
+    <$Wrapper withAction={Boolean(action)}>
+      <$WrapperInner withAction={Boolean(action)}>
+        {action && (
+          <$ActionLeft>
+            <$CheckIcon size="m" />
+          </$ActionLeft>
+        )}
+        <$Section paddingBottom={Boolean(action)}>
+          {header && (
+            <Heading
+              header={header}
+              loading={loading}
+              as="h2"
+              size="s"
+              {...rest}
+            />
+          )}
+          {children && <$Grid role={role}>{children}</$Grid>}
+          {!action && (
+            <$Hr
+              css={`
+                margin-top: ${theme.spacing.l};
+              `}
+            />
+          )}
+        </$Section>
+      </$WrapperInner>
+      {action && <$ActionBottom>{action}</$ActionBottom>}
+    </$Wrapper>
+  );
+};
+
+export default ReviewSection;
