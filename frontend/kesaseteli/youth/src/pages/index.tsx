6b45f73d73ae3e3bb8912ab058a338b2f37f5564
Jaakko Nenonen <jaakko@codelicious.fi>
2021-11-22 10:02:39 +0200

    add title and container

--- frontend/kesaseteli/youth/src/pages/index.tsx
+++ frontend/kesaseteli/youth/src/pages/index.tsx
@@ -1,11 +1,22 @@
+import { Container } from 'hds-react';
 import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
 import { useTranslation } from 'next-i18next';
 import React from 'react';
+import Heading from 'shared/components/forms/heading/Heading';
 import getServerSideTranslations from 'shared/i18n/get-server-side-translations';
 
 const YouthIndex: NextPage = () => {
   const { t } = useTranslation();
-  return <p>Hello, {t('common:appName')}!</p>;
+  return (
+    <Container>
+      <Head>
+        <title>{t(`common:appName`)}</title>
+      </Head>
+      <Heading size="xl" header={t('common:applicationPage.title')} as="h2" />
+      <p>{t('common:applicationPage.paragraph_1')}</p>
+    </Container>
+  );
 };
 
 export const getStaticProps: GetStaticProps =
