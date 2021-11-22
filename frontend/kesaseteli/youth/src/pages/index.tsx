277dde93a721daa17cc7b570e3b0d9acc783de2b
Jaakko Nenonen <jaakko@codelicious.fi>
2021-11-22 17:46:12 +0200

    YJDH-235: make layout responsive

--- frontend/kesaseteli/youth/src/pages/index.tsx
+++ frontend/kesaseteli/youth/src/pages/index.tsx
@@ -1,8 +1,8 @@
-import { Container } from 'hds-react';
 import { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import { useTranslation } from 'next-i18next';
 import React from 'react';
+import Container from 'shared/components/container/Container';
 import Heading from 'shared/components/forms/heading/Heading';
 import getServerSideTranslations from 'shared/i18n/get-server-side-translations';
 
@@ -13,7 +13,7 @@ const YouthIndex: NextPage = () => {
       <Head>
         <title>{t(`common:appName`)}</title>
       </Head>
-      <Heading size="xl" header={t('common:applicationPage.title')} as="h2" />
+      <Heading size="l" header={t('common:applicationPage.title')} as="h2" />
       <p>{t('common:applicationPage.paragraph_1')}</p>
     </Container>
   );
