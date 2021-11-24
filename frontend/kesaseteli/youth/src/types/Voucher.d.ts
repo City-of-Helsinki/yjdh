16fbb9f8604a741aa185256b5238bfd220d3b831
Jaakko Nenonen <jaakko@codelicious.fi>
2021-11-24 11:04:16 +0200

    YJDH-235: form skeleton

---
+++ frontend/kesaseteli/youth/src/types/Voucher.d.ts
@@ -0,0 +1,12 @@
+type Voucher = {
+  firstName: string;
+  lastName: string;
+  ssn: string;
+  postcode: string;
+  school: string;
+  phoneNumber: string;
+  email: string;
+  termsAndConditions: boolean;
+};
+
+export default Voucher;
