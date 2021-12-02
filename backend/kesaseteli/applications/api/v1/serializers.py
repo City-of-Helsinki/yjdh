b8ef52670032a6cda9fd4d68fef1a1be66e0792a
Kari Salminen <kari.salminen@anders.com>
2021-12-02 10:47:28 +0200

    Add /v1/schools/ endpoint with hardcoded data
    Add tests for the /v1/schools/ endpoint:
     - test_schools_list_status_ok
     - test_schools_list_returns_list
     - test_schools_list_returns_non_empty
     - test_schools_list_returns_active_school_count
     - test_schools_list_returns_string_collection
     - test_schools_list_returns_non_empty_strings_collection
     - test_schools_list_returns_unique_elements_collection
     - test_schools_list_returns_sorted_collection
    
    NOTE: The hardcoded school values in /v1/schools/ are placeholder data.
    
    Refs YJDH-335

--- backend/kesaseteli/applications/api/v1/serializers.py
+++ backend/kesaseteli/applications/api/v1/serializers.py
@@ -11,7 +11,13 @@ from applications.enums import (
     AttachmentType,
     SummerVoucherExceptionReason,
 )
-from applications.models import Application, Attachment, SummerVoucher, YouthApplication
+from applications.models import (
+    Application,
+    Attachment,
+    School,
+    SummerVoucher,
+    YouthApplication,
+)
 from companies.api.v1.serializers import CompanySerializer
 from companies.services import get_or_create_company_using_organization_roles
 
@@ -421,6 +427,16 @@ class ApplicationSerializer(serializers.ModelSerializer):
                         attachment_types=", ".join(required_attachment_types),
                     )
                 )
+
+
+class SchoolSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        return instance.name
+
+    class Meta:
+        model = School
+        fields = ["name"]
+        read_only_fields = ["name"]
 
 
 class YouthApplicationSerializer(serializers.ModelSerializer):
---
+++ backend/kesaseteli/applications/tests/test_schools_api.py
@@ -0,0 +1,56 @@
+import pytest
+from rest_framework import status
+
+from applications.models import School
+
+
+def get_schools_api_url():
+    return "/v1/schools/"
+
+
+@pytest.mark.django_db
+def test_schools_list_status_ok(api_client):
+    response = api_client.get(get_schools_api_url())
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_schools_list_returns_list(api_client):
+    response = api_client.get(get_schools_api_url())
+    assert type(response.json()) == list
+
+
+@pytest.mark.django_db
+def test_schools_list_returns_non_empty(api_client):
+    response = api_client.get(get_schools_api_url())
+    assert len(response.json()) > 0
+
+
+@pytest.mark.django_db
+def test_schools_list_returns_active_school_count(api_client):
+    response = api_client.get(get_schools_api_url())
+    assert len(response.json()) == len(School.objects.active())
+
+
+@pytest.mark.django_db
+def test_schools_list_returns_string_collection(api_client):
+    response = api_client.get(get_schools_api_url())
+    assert all(type(element) == str for element in response.json())
+
+
+@pytest.mark.django_db
+def test_schools_list_returns_non_empty_strings_collection(api_client):
+    response = api_client.get(get_schools_api_url())
+    assert all(len(element.strip()) > 0 for element in response.json())
+
+
+@pytest.mark.django_db
+def test_schools_list_returns_unique_elements_collection(api_client):
+    response = api_client.get(get_schools_api_url())
+    assert len(set(response.json())) == len(response.json())
+
+
+@pytest.mark.django_db
+def test_schools_list_returns_sorted_collection(api_client):
+    response = api_client.get(get_schools_api_url())
+    assert sorted(response.json()) == response.json()
