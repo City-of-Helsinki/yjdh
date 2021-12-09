d0762a3bb3483d746a2dcbefcc1c7985ed2a9f2e
Matti Korttila <matti.korttila@sysart.fi>
2021-12-09 15:29:09 +0200

    fixed addressed automatic check failures

--- backend/tet/tet/settings.py
+++ backend/tet/tet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v4240nrl*8^ij&3!h2!hhzp%v1b0zmo@qwl6#r9w%_4(*7gm*b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
