bd1bdd1e43e15320cd982de087bb25f1f63d3c21
Matti Korttila <matti.korttila@sysart.fi>
2021-12-13 15:39:52 +0200

    modify settings.py, include shared apps
    The test command used in pytest flow needs this.

--- backend/tet/tet/settings.py
+++ backend/tet/tet/settings.py
@@ -10,43 +10,148 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os
+import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+checkout_dir = environ.Path(__file__) - 2
+assert os.path.exists(checkout_dir("manage.py"))
 
+BASE_DIR = str(checkout_dir)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+parent_dir = checkout_dir.path("..")
+if os.path.isdir(parent_dir("etc")):
+    env_file = parent_dir("etc/env")
+    default_var_root = environ.Path(parent_dir("var"))
+else:
+    env_file = checkout_dir(".env.tet")
+    default_var_root = environ.Path(checkout_dir("var"))
 
-SECRET_KEY = "xxxx"
+env = environ.Env(
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, ""),
+    MEDIA_ROOT=(environ.Path(), default_var_root("media")),
+    STATIC_ROOT=(environ.Path(), default_var_root("static")),
+    MEDIA_URL=(str, "/media/"),
+    STATIC_URL=(str, "/static/"),
+    ALLOWED_HOSTS=(list, ["*"]),
+    USE_X_FORWARDED_HOST=(bool, False),
+    DATABASE_URL=(
+        str,
+        "postgres:///tet",
+    ),
+    CACHE_URL=(str, "locmemcache://"),
+    SENTRY_DSN=(str, ""),
+    SENTRY_ENVIRONMENT=(str, ""),
+    CORS_ALLOWED_ORIGINS=(list, []),
+    CORS_ALLOW_ALL_ORIGINS=(bool, False),
+    CSRF_COOKIE_DOMAIN=(str, "localhost"),
+    CSRF_TRUSTED_ORIGINS=(list, []),
+    MOCK_FLAG=(bool, False),
+    SESSION_COOKIE_AGE=(int, 60 * 60 * 2),
+    OIDC_RP_CLIENT_ID=(str, ""),
+    OIDC_RP_CLIENT_SECRET=(str, ""),
+    OIDC_OP_BASE_URL=(str, ""),
+    LOGIN_REDIRECT_URL=(str, "/"),
+    LOGIN_REDIRECT_URL_FAILURE=(str, "/"),
+    ADFS_LOGIN_REDIRECT_URL=(str, "/excel-download/"),
+    ADFS_LOGIN_REDIRECT_URL_FAILURE=(str, "/"),
+    EAUTHORIZATIONS_BASE_URL=(str, ""),
+    EAUTHORIZATIONS_CLIENT_ID=(str, ""),
+    EAUTHORIZATIONS_CLIENT_SECRET=(str, ""),
+    EAUTHORIZATIONS_API_OAUTH_SECRET=(str, ""),
+    ADFS_CLIENT_ID=(str, "client_id"),
+    ADFS_CLIENT_SECRET=(str, "client_secret"),
+    ADFS_TENANT_ID=(str, "tenant_id"),
+    ADFS_CONTROLLER_GROUP_UUIDS=(list, []),
+    DEFAULT_FILE_STORAGE=(str, "django.core.files.storage.FileSystemStorage"),
+    AZURE_ACCOUNT_NAME=(str, ""),
+    AZURE_ACCOUNT_KEY=(str, ""),
+    AZURE_CONTAINER=(str, ""),
+    AUDIT_LOG_ORIGIN=(str, ""),
+    # Random 32 bytes AES key, for testing purpose only, DO NOT use it value in staging/production
+    # Always override this value from env variables
+    ENCRYPTION_KEY=(
+        str,
+        "f164ec6bd6fbc4aef5647abc15199da0f9badcc1d2127bde2087ae0d794a9a0b",
+    ),
+    SOCIAL_SECURITY_NUMBER_HASH_KEY=(
+        str,
+        "ee235e39ebc238035a6264c063dd829d4b6d2270604b57ee1f463e676ec44669",
+    ),
+    CLEAR_AUDIT_LOG_ENTRIES=(bool, False),
+    ENABLE_SEND_AUDIT_LOG=(bool, False),
+    ENABLE_ADMIN=(bool, True),
+    DB_PREFIX=(str, ""),
+)
+
+if os.path.exists(env_file):
+    env.read_env(env_file)
+
+BASE_DIR = str(checkout_dir)
+
+DEBUG = env.bool("DEBUG")
+SECRET_KEY = env.str("SECRET_KEY")
+if DEBUG and not SECRET_KEY:
+    SECRET_KEY = "xxxx"
+
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+DATABASES = {"default": env.db()}
+
+CACHES = {"default": env.cache()}
+
+MEDIA_ROOT = env("MEDIA_ROOT")
+STATIC_ROOT = env("STATIC_ROOT")
+MEDIA_URL = env.str("MEDIA_URL")
+STATIC_URL = env.str("STATIC_URL")
+ENABLE_ADMIN = env.bool("ENABLE_ADMIN")
+ENCRYPTION_KEY = env.str("ENCRYPTION_KEY")
+
+ROOT_URLCONF = "tet.urls"
+WSGI_APPLICATION = "tet.wsgi.application"
+
+LANGUAGE_CODE = "fi"
+TIME_ZONE = "Europe/Helsinki"
+USE_TZ = True
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
+    "mozilla_django_oidc",
+    "django_extensions",
+    "django_auth_adfs",
+    # shared apps
+    "shared.audit_log",
+    "shared.oidc",
+    # local apps
 ]
+
+if ENABLE_ADMIN:
+    INSTALLED_APPS.append("django.contrib.admin")
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_history.middleware.HistoryRequestMiddleware",
 ]
-
-ROOT_URLCONF = "tet.urls"
 
 TEMPLATES = [
     {
@@ -59,64 +164,120 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ],
+            ]
+        },
+    }
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
+CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS")
+CSRF_COOKIE_DOMAIN = env.str("CSRF_COOKIE_DOMAIN")
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
+CSRF_COOKIE_SECURE = True
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s p%(process)d %(name)s %(levelname)s: %(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
     },
-]
-
-WSGI_APPLICATION = "tet.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "WARNING"},
+    },
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
+}
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+# Mock flag for testing purposes
+MOCK_FLAG = env.bool("MOCK_FLAG")
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
+# Authentication
+SESSION_COOKIE_AGE = env.int("SESSION_COOKIE_AGE")
+SESSION_COOKIE_SECURE = True
 
+AUTHENTICATION_BACKENDS = (
+    "shared.oidc.auth.HelsinkiOIDCAuthenticationBackend",
+    "shared.azure_adfs.auth.HelsinkiAdfsAuthCodeBackend",
+    "django.contrib.auth.backends.ModelBackend",
+)
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
+OIDC_RP_SIGN_ALGO = "RS256"
+OIDC_RP_SCOPES = "openid profile"
 
-LANGUAGE_CODE = "en-us"
+OIDC_RP_CLIENT_ID = env.str("OIDC_RP_CLIENT_ID")
+OIDC_RP_CLIENT_SECRET = env.str("OIDC_RP_CLIENT_SECRET")
 
-TIME_ZONE = "UTC"
+OIDC_OP_BASE_URL = env.str("OIDC_OP_BASE_URL")
+OIDC_OP_AUTHORIZATION_ENDPOINT = f"{OIDC_OP_BASE_URL}/auth"
+OIDC_OP_TOKEN_ENDPOINT = f"{OIDC_OP_BASE_URL}/token"
+OIDC_OP_USER_ENDPOINT = f"{OIDC_OP_BASE_URL}/userinfo"
+OIDC_OP_JWKS_ENDPOINT = f"{OIDC_OP_BASE_URL}/certs"
+OIDC_OP_LOGOUT_ENDPOINT = f"{OIDC_OP_BASE_URL}/logout"
 
-USE_I18N = True
+LOGIN_REDIRECT_URL = env.str("LOGIN_REDIRECT_URL")
+LOGIN_REDIRECT_URL_FAILURE = env.str("LOGIN_REDIRECT_URL_FAILURE")
 
-USE_L10N = True
+EAUTHORIZATIONS_BASE_URL = env.str("EAUTHORIZATIONS_BASE_URL")
+EAUTHORIZATIONS_CLIENT_ID = env.str("EAUTHORIZATIONS_CLIENT_ID")
+EAUTHORIZATIONS_CLIENT_SECRET = env.str("EAUTHORIZATIONS_CLIENT_SECRET")
+EAUTHORIZATIONS_API_OAUTH_SECRET = env.str("EAUTHORIZATIONS_API_OAUTH_SECRET")
 
-USE_TZ = True
+# Azure ADFS
+LOGIN_URL = "django_auth_adfs:login"
 
+ADFS_CLIENT_ID = env.str("ADFS_CLIENT_ID") or "client_id"
+ADFS_CLIENT_SECRET = env.str("ADFS_CLIENT_SECRET") or "client_secret"
+ADFS_TENANT_ID = env.str("ADFS_TENANT_ID") or "tenant_id"
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+# https://django-auth-adfs.readthedocs.io/en/latest/azure_ad_config_guide.html#step-2-configuring-settings-py
+AUTH_ADFS = {
+    "AUDIENCE": ADFS_CLIENT_ID,
+    "CLIENT_ID": ADFS_CLIENT_ID,
+    "CLIENT_SECRET": ADFS_CLIENT_SECRET,
+    "CLAIM_MAPPING": {"email": "email"},
+    "USERNAME_CLAIM": "oid",
+    "TENANT_ID": ADFS_TENANT_ID,
+    "RELYING_PARTY_ID": ADFS_CLIENT_ID,
+}
 
-STATIC_URL = "/static/"
+ADFS_LOGIN_REDIRECT_URL = env.str("ADFS_LOGIN_REDIRECT_URL")
+ADFS_LOGIN_REDIRECT_URL_FAILURE = env.str("ADFS_LOGIN_REDIRECT_URL_FAILURE")
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+ADFS_CONTROLLER_GROUP_UUIDS = env.list("ADFS_CONTROLLER_GROUP_UUIDS")
+# End of Authentication
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+FIELD_ENCRYPTION_KEYS = [ENCRYPTION_KEY]
+
+# Django storages
+DEFAULT_FILE_STORAGE = env("DEFAULT_FILE_STORAGE")
+
+AZURE_ACCOUNT_NAME = env("AZURE_ACCOUNT_NAME")
+AZURE_ACCOUNT_KEY = env("AZURE_ACCOUNT_KEY")
+AZURE_CONTAINER = env("AZURE_CONTAINER")
+
+MAX_UPLOAD_SIZE = 10485760  # 10MB
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+local_settings_path = os.path.join(checkout_dir(), "local_settings.py")
+if os.path.exists(local_settings_path):
+    with open(local_settings_path) as fp:
+        code = compile(fp.read(), local_settings_path, "exec")
+    exec(code, globals(), locals())
