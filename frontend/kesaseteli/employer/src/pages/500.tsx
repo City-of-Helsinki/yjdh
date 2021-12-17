cafde8de7be17c3a218ed04028f443c92f499442
Jaakko Nenonen <jaakko@codelicious.fi>
2021-12-17 22:51:52 +0200

    add test to check the routing to thank you -page

--- frontend/kesaseteli/employer/src/pages/500.tsx
+++ frontend/kesaseteli/employer/src/pages/500.tsx
@@ -3,8 +3,8 @@ import { GetStaticProps, NextPage } from 'next';
 import React from 'react';
 import withAuth from 'shared/components/hocs/withAuth';
 import ServerErrorPage from 'shared/components/pages/ServerErrorPage';
-import getServerSideTranslations from 'shared/i18n/get-server-side-translations';
 import useErrorHandler from 'shared/hooks/useErrorHandler';
+import getServerSideTranslations from 'shared/i18n/get-server-side-translations';
 
 const FiveHundred: NextPage = () => {
   const logoutQuery = useLogoutQuery();
