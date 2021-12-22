9acd52ba9cd89e89155df58ed84ace1488cb7594
Ivan Melnik <melnik.job@gmail.com>
2021-12-22 09:23:46 +0100

    HL-270: Handlers edit action (#561)

--- frontend/benefit/handler/src/components/applicationReview/ApplicationReview.tsx
+++ frontend/benefit/handler/src/components/applicationReview/ApplicationReview.tsx
@@ -5,10 +5,12 @@ import * as React from 'react';
 import Container from 'shared/components/container/Container';
 import StickyActionBar from 'shared/components/stickyActionBar/StickyActionBar';
 import { $StickyBarSpacing } from 'shared/components/stickyActionBar/StickyActionBar.sc';
+import { convertToUIDateFormat } from 'shared/utils/date.utils';
 
 import HandlingApplicationActions from './actions/handlingApplicationActions/HandlingApplicationActions';
 import ReceivedApplicationActions from './actions/receivedApplicationActions/ReceivedApplicationActions';
 import ApplicationProcessingView from './applicationProcessingView/AplicationProcessingView';
+import { $DataValueAlert } from './ApplicationReview.sc';
 import BenefitView from './benefitView/BenefitView';
 import CompanyInfoView from './companyInfoView/CompanyInfoView';
 import ConsentView from './consentView/ConsentView';
@@ -22,8 +24,7 @@ import PaySubsidyView from './paySubsidyView/PaySubsidyView';
 import { useApplicationReview } from './useApplicationReview';
 
 const ApplicationReview: React.FC = () => {
-  const { application, isLoading } = useApplicationReview();
-
+  const { application, isLoading, t } = useApplicationReview();
   if (isLoading) {
     return (
       <Container>
@@ -36,6 +37,15 @@ const ApplicationReview: React.FC = () => {
     <>
       <ApplicationHeader data={application} />
       <Container>
+        {application.status === APPLICATION_STATUSES.INFO_REQUIRED && (
+          <$DataValueAlert>
+            {t(`common:review.fields.editEndDate`, {
+              date: convertToUIDateFormat(
+                application.additionalInformationNeededBy
+              ),
+            })}
+          </$DataValueAlert>
+        )}
         <CompanyInfoView data={application} />
         <ContactPersonView data={application} />
         <DeminimisView data={application} />
@@ -56,7 +66,8 @@ const ApplicationReview: React.FC = () => {
         {application.status === APPLICATION_STATUSES.RECEIVED && (
           <ReceivedApplicationActions application={application} />
         )}
-        {application.status === APPLICATION_STATUSES.HANDLING && (
+        {(application.status === APPLICATION_STATUSES.HANDLING ||
+          application.status === APPLICATION_STATUSES.INFO_REQUIRED) && (
           <HandlingApplicationActions application={application} />
         )}
       </StickyActionBar>
