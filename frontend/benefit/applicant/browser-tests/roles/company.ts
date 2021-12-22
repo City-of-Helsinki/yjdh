2748202862dec6dd902d1749fdc609eb804459c5
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-12-22 12:58:20 +0200

    Add company role to borwser tests

---
+++ frontend/benefit/applicant/browser-tests/roles/company.ts
@@ -0,0 +1,18 @@
+import { Role } from 'testcafe';
+import login from '../page-modal/login';
+import { getFrontendUrl } from '../utils/url.utils';
+
+const url = getFrontendUrl('/');
+
+const company = Role(url, async (t) => {
+  await t
+    .click(login.loginButton)
+    .click(login.loginOptionButton)
+    .click(login.defaultSSN)
+    .click(login.authenticateButton)
+    .click(login.continueButton)
+    .click(login.companyRadioButton)
+    .click(login.submitButton);
+});
+
+export default company;
