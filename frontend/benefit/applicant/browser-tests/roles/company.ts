4ddc49530b5514ec876918f115fb5f1f00def1f3
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-12-23 13:39:20 +0200

    Disable authentication in review env

--- frontend/benefit/applicant/browser-tests/roles/company.ts
+++ frontend/benefit/applicant/browser-tests/roles/company.ts
@@ -5,14 +5,13 @@ import { getFrontendUrl } from '../utils/url.utils';
 const url = getFrontendUrl('/');
 
 const company = Role(url, async (t) => {
-  await t
-    .click(login.loginButton)
-    .click(login.loginOptionButton)
-    .click(login.defaultSSN)
-    .click(login.authenticateButton)
-    .click(login.continueButton)
-    .click(login.companyRadioButton)
-    .click(login.submitButton);
+  await t.click(login.loginButton);
+  // .click(login.loginOptionButton)
+  // .click(login.defaultSSN)
+  // .click(login.authenticateButton)
+  // .click(login.continueButton)
+  // .click(login.companyRadioButton)
+  // .click(login.submitButton);
 });
 
 export default company;
