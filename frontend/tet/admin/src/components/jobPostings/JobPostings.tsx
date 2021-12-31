29767428cb89acd2eff57fc32498dcc0ee0c8cdd
Matti Korttila <matti.korttila@sysart.fi>
2021-12-31 11:24:39 +0200

    add data fetching logic

--- frontend/tet/admin/src/components/jobPostings/JobPostings.tsx
+++ frontend/tet/admin/src/components/jobPostings/JobPostings.tsx
@@ -4,16 +4,29 @@ import Container from 'shared/components/container/Container';
 import { useTranslation } from 'next-i18next';
 
 import { $Heading, $HeadingContainer } from './JobPostings.sc';
+import { useQuery } from 'react-query';
+import { BackendEndpoint } from 'tet-shared/backend-api/backend-api';
+import PageLoadingSpinner from 'shared/components/pages/PageLoadingSpinner';
+import { useRouter } from 'next/router';
 
 const JobPostings: React.FC = () => {
   const { t } = useTranslation();
+  const { isLoading, data } = useQuery(BackendEndpoint.TET_POSTINGS);
+  const router = useRouter();
+
+  if (isLoading) {
+    return <PageLoadingSpinner />;
+  }
+
+  console.dir(data);
+
   return (
     <Container>
       <$HeadingContainer>
         <$Heading>{t('common:application.jobPostings.title')}</$Heading>
       </$HeadingContainer>
       <p>{t('common:application.jobPostings.noPostingsFound')}</p>
-      <Button>{t('common:application.jobPostings.addNewPosting')}</Button>
+      <Button onClick={() => router.push('/new')}>{t('common:application.jobPostings.addNewPosting')}</Button>
     </Container>
   );
 };
