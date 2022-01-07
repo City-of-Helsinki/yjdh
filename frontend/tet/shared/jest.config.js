8445c70fe32c60b77b41d496db99b5f5a7bf4f70
Matti Korttila <matti.korttila@sysart.fi>
2022-01-07 12:38:36 +0200

    fix tet-shared jest config

--- frontend/tet/shared/jest.config.js
+++ frontend/tet/shared/jest.config.js
@@ -7,8 +7,8 @@ module.exports = {
     },
   },
   moduleNameMapper: {
-    ['^kesaseteli/shared/test/(.*)$']: '<rootDir>/test/$1',
-    [`^kesaseteli/shared\/(.*)$`]: '<rootDir>src/$1',
+    ['^tet/shared/test/(.*)$']: '<rootDir>/test/$1',
+    [`^tet/shared\/(.*)$`]: '<rootDir>src/$1',
   },
   setupFilesAfterEnv: [
     '<rootDir>/../../shared/src/__tests__/utils/setupTests.ts',
