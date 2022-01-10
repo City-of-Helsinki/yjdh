f479e4097f8d65c9ceb2471038c42451fbdb7311
Matti Korttila <matti.korttila@gmail.com>
2022-01-10 14:55:01 +0200

    TETP-20: TET-admin layout and i18n (#562)
    * add initial layout and stuctures to tet/admin frontend
    * add error pages, cleanup
    * add links to footer
    * add docker-compose for tet-admin (for better DX)
    * add first tests
    * add data fetching logic
    * add stub endpoint returning hardcoded data to backend
    * add new.tsx, start Editor component

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -9,6 +9,9 @@ module.exports = {
     [`^kesaseteli/handler\/(.*)$`]: '<rootDir>/kesaseteli/handler/src/$1',
     [`^benefit/applicant\/(.*)$`]: '<rootDir>/benefit/applicant/src/$1',
     [`^benefit/handler\/(.*)$`]: '<rootDir>/benefit/handler/src/$1',
+    [`^tet/admin\/(.*)$`]: '<rootDir>/tet/admin/src/$1',
+    [`^tet/employer\/(.*)$`]: '<rootDir>/tet/employer/src/$1',
+    [`^tet/youth\/(.*)$`]: '<rootDir>/tet/youth/src/$1',
   },
   setupFilesAfterEnv: [
     '<rootDir>/shared/src/__tests_/utils/setupTests.ts',
@@ -16,6 +19,7 @@ module.exports = {
     '<rootDir>benefit/handler/test/i18n/i18n-test.ts',
     '<rootDir>kesaseteli/employer/src/__tests__/utils/i18n/i18n-test.ts',
     '<rootDir>kesaseteli/youth/src/__tests__/utils/i18n/i18n-test.ts',
+    '<rootDir>tet/admin/src/__tests__/utils/init-18n.ts',
   ],
   coveragePathIgnorePatterns: [
     '<rootDir>/kesaseteli/employer/src/pages/',
@@ -26,6 +30,9 @@ module.exports = {
     '<rootDir>/shared/src/server/next-server.js',
     '<rootDir>/shared/src/__tests__/component-apis/',
     '<rootDir>/shared/src/__tests__/utils/',
+    '<rootDir>/tet/admin/src/pages/',
+    '<rootDir>/tet/employer/src/pages/',
+    '<rootDir>/tet/youth/src/pages/',
   ],
   collectCoverageFrom: ['./**/*.{ts,tsx,js,jsx}'],
   testPathIgnorePatterns: ['__tests__/utils', 'component-apis'],
