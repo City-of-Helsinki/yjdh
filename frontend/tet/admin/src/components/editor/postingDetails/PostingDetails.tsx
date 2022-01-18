61ddb14f077ba6ce3fc4aa998cd22ebe17af7981
Matti Korttila <matti.korttila@sysart.fi>
2022-01-18 17:54:52 +0200

    add form-aware NumberInput

--- frontend/tet/admin/src/components/editor/postingDetails/PostingDetails.tsx
+++ frontend/tet/admin/src/components/editor/postingDetails/PostingDetails.tsx
@@ -3,12 +3,12 @@ import FormSection from 'shared/components/forms/section/FormSection';
 import { $Grid, $GridCell } from 'shared/components/forms/section/FormSection.sc';
 import { useTranslation } from 'next-i18next';
 import { useTheme } from 'styled-components';
-import { NumberInput } from 'hds-react';
 import { $CompanyInfoRow } from 'tet/admin/components/editor/companyInfo/CompanyInfo.sc';
 import DateInput from 'shared/components/forms/inputs/DateInput';
 import TetPosting from 'tet/admin/types/tetposting';
 import TextInput from 'shared/components/forms/inputs/TextInput';
 import { EditorSectionProps } from 'tet/admin/components/editor/Editor';
+import NumberInput from 'tet/admin/components/editor/NumberInput';
 
 const PostingDetails: React.FC<EditorSectionProps> = ({ initialValue }) => {
   const { t } = useTranslation();
@@ -52,14 +52,7 @@ const PostingDetails: React.FC<EditorSectionProps> = ({ initialValue }) => {
         <$CompanyInfoRow>{t('common:editor.posting.workHoursNotice')}</$CompanyInfoRow>
       </$GridCell>
       <$GridCell $colSpan={2}>
-        <NumberInput
-          id="job-count-selector"
-          label={t('common:editor.posting.spotsLabel')}
-          minusStepButtonAriaLabel="Decrease by one"
-          plusStepButtonAriaLabel="Increase by one"
-          step={1}
-          defaultValue={initialValue.spots}
-        />
+        <NumberInput id="spots" label={t('common:editor.posting.spotsLabel')} initialValue={initialValue.spots} />
       </$GridCell>
       <$GridCell as={$Grid} $colSpan={12}>
         <$GridCell $colSpan={6}>
--- frontend/tet/admin/src/types/tetposting.d.ts
+++ frontend/tet/admin/src/types/tetposting.d.ts
@@ -2,6 +2,8 @@ type TetPosting = {
   id?: string;
   title: string;
   description: string;
+  // TODO react-hook-forms gets this as a string from `NumberInput`
+  // but NumberInput expects it as a number
   spots: number;
   start_date: string;
   end_date?: string;
