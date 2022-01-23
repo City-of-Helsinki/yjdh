07d953c754fc2c5bd5593e14b0f410ff0ea7e41e
Ivan Melnik <melnik.job@gmail.com>
2022-01-23 14:52:14 +0200

    HL-372: Applicant Message indicator (#675)

--- frontend/benefit/applicant/src/components/applications/applicationList/ApplicationList.sc.ts
+++ frontend/benefit/applicant/src/components/applications/applicationList/ApplicationList.sc.ts
@@ -12,5 +12,23 @@ export const $ListWrapper = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
+`;
+
+export const $ListInfo = styled.div`
+  display: grid;
+  grid-template-columns: 60px 3fr;
+  grid-gap: ${(props) => props.theme.spacing.m};
+  background-color: ${(props) => props.theme.colors.coatOfArms};
+  padding: ${(props) => props.theme.spacing.xs2};
+  color: ${(props) => props.theme.colors.white};
   margin-bottom: ${(props) => props.theme.spacing.l};
+`;
+
+export const $ListInfoInner = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const $ListInfoText = styled.div`
+  padding: 0 ${(props) => props.theme.spacing.xs2};
 `;
