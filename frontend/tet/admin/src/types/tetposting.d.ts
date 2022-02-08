4eee282d02f9a60048099241f34709504811e0aa
Matti Korttila <matti.korttila@sysart.fi>
2022-02-08 13:31:01 +0200

    remove duplicate fields

--- frontend/tet/admin/src/types/tetposting.d.ts
+++ frontend/tet/admin/src/types/tetposting.d.ts
@@ -3,13 +3,6 @@ type TetPosting = {
   title: string;
   description: string;
   org_name: string;
-  // TODO react-hook-forms gets this as a string from `NumberInput`
-  // but NumberInput expects it as a number
-  contact_first_name: string;
-  contact_last_name: string;
-  contact_phone: string;
-  contact_email: string;
-  contact_language: string;
   spots: number;
   start_date: string;
   end_date?: string;
