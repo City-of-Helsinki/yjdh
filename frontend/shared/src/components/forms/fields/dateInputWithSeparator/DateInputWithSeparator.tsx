cb5908cb6412d7fb8a8fb80e62ae9ca4af538bd7
ngoc <ngocdh236@gmail.com>
2022-02-17 09:09:27 +0200

    Fix lint issue

--- frontend/shared/src/components/forms/fields/dateInputWithSeparator/DateInputWithSeparator.tsx
+++ frontend/shared/src/components/forms/fields/dateInputWithSeparator/DateInputWithSeparator.tsx
@@ -1,20 +1,19 @@
 import { DateInput, DateInputProps } from 'hds-react';
+import React from 'react';
 
-const DateInputWithSeparator: React.FC<DateInputProps> = (props) => {
-  return (
-    <DateInput
-      {...props}
-      css={`
-        display: flex;
-        align-items: center;
-        :after {
-          margin-left: var(--spacing-s);
-          content: '-';
-          font-weight: bold;
-        }
-      `}
-    ></DateInput>
-  );
-};
+const DateInputWithSeparator: React.FC<DateInputProps> = (props) => (
+  <DateInput
+    {...props}
+    css={`
+      display: flex;
+      align-items: center;
+      :after {
+        margin-left: var(--spacing-s);
+        content: '-';
+        font-weight: bold;
+      }
+    `}
+  />
+);
 
 export default DateInputWithSeparator;
