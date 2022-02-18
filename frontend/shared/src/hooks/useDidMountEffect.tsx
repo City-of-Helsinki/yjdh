8f0e476ecef280d4f7a758a0874208df572715e5
ngoc <ngocdh236@gmail.com>
2022-02-18 15:14:51 +0200

    Fix lint issues

--- frontend/shared/src/hooks/useDidMountEffect.tsx
+++ frontend/shared/src/hooks/useDidMountEffect.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from 'react';
 
-const useDidMountEffect = (func: Function, deps: Array<any>) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useDidMountEffect = (func: () => void, deps: Array<any>): void => {
   const didMount = useRef(false);
   useEffect(() => {
     if (didMount.current) func();
     else didMount.current = true;
-  }, deps);
+  }, deps); // eslint-disable-line react-hooks/exhaustive-deps
 };
 
 export default useDidMountEffect;
