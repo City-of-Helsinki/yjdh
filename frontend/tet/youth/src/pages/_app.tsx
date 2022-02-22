61e4c772c279b7e39de3759402e8c0e15495c17f
Matti Korttila <matti.korttila@sysart.fi>
2022-02-22 15:47:04 +0200

    use BaseApp, add navigation

--- frontend/tet/youth/src/pages/_app.tsx
+++ frontend/tet/youth/src/pages/_app.tsx
@@ -1,20 +1,18 @@
 // import 'react-toastify/dist/ReactToastify.css';
 
-import Footer from 'tet/admin/components/footer/Footer';
-import Header from 'tet/admin/components/header/Header';
-import { linkedEventsUrl } from 'tet/youth/backend-api/backend-api';
 import createQueryClient from 'tet/youth/query-client/create-query-client';
 import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import React from 'react';
 import { QueryClientProvider } from 'react-query';
-import BackendAPIProvider from 'shared/backend-api/BackendAPIProvider';
 import BaseApp from 'shared/components/app/BaseApp';
 import YouthApp from 'tet/youth/components/YouthApp';
+import Header from 'tet/youth/components/header/Header';
+import Footer from 'tet/youth/components/footer/Footer';
 
 const App: React.FC<AppProps> = (appProps) => (
   <QueryClientProvider client={createQueryClient()}>
-    <YouthApp {...appProps} />
+    <BaseApp header={<Header />} footer={<Footer />} {...appProps} />
   </QueryClientProvider>
 );
 
