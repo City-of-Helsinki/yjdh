c8ae2cd59487090ede5d815f30c44a4e5931b500
Ville-Matias Pekkarinen <ville-matias.pekkarinen@vincit.fi>
2022-02-25 10:35:00 +0200

    Show more button

--- frontend/tet/youth/src/components/eventList/EventList.tsx
+++ frontend/tet/youth/src/components/eventList/EventList.tsx
@@ -5,10 +5,13 @@ import PageLoadingSpinner from 'shared/components/pages/PageLoadingSpinner';
 import { LinkedEventsPagedResponse, LocalizedObject, TetEvent } from 'tet/youth/linkedevents';
 import { useRouter } from 'next/router';
 import JobPostingCard from 'tet/youth/components/eventList/JobPostingCard/JobPostingCard';
+import { useTranslation } from 'next-i18next';
+import { Button } from 'hds-react';
 
 const getLocalizedString = (obj: LocalizedObject): string => obj.fi;
 
 const EventList: React.FC = () => {
+  const { t } = useTranslation();
   const { isLoading, data, error } = useQuery<LinkedEventsPagedResponse<TetEvent>>(BackendEndpoint.EVENT);
   const router = useRouter();
 
@@ -26,6 +29,7 @@ const EventList: React.FC = () => {
     <div>
       <h1>Tet-paikat testi</h1>
       {data && data.data.map((e) => <JobPostingCard key={e.id} jobPosting={e} />)}
+      <Button>{t('common:postingsPage.showMore')}</Button>
     </div>
   );
 };
