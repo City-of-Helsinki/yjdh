f7dc71821785b80ce5c64a5270bad13a18895995
Jaakko Nenonen <jaakko@codelicious.fi>
2022-02-28 16:16:36 +0200

    YJDH-424: use base eslint configuraiton + fix errors + fix dockefile paths

---
+++ frontend/.eslintrc.base.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  extends: ['auto', 'plugin:you-dont-need-lodash-underscore/compatible'],
+  rules: {
+    'no-secrets/no-secrets': [
+      'error',
+      { ignoreContent: 'https://makasiini.hel.ninja/delivery' },
+    ],
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/prefer-export-from': 'off',
+    'lodash/prefer-noop': 'off',
+    'sonarjs/no-nested-template-literals': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        'import/prefer-default-export': 'off',
+      },
+    },
+    {
+      files: ['**/__tests__/**', '*.testcafe.ts'],
+      rules: {
+        'testing-library/render-result-naming-convention': 'off',
+        'jest/expect-expect': 'off',
+        'jest/no-done-callback': 'off',
+        'security/detect-non-literal-regexp': 'off',
+        'no-secrets/no-secrets': 'off',
+        'no-await-in-loop': 'off',
+        'no-restricted-syntax': 0,
+      },
+    },
+    {
+      files: ['*.components.ts'],
+      rules: {
+        'security/detect-non-literal-fs-filename': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'testing-library/await-async-query': 'off',
+        'security/detect-non-literal-regexp': 'off',
+        'no-secrets/no-secrets': 'off',
+      },
+    },
+  ],
+};
