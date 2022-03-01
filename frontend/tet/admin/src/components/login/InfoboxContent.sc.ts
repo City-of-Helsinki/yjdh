8615d2f7c2d2c44120fe9db11f77eaf7f578cc98
Matti Korttila <matti.korttila@sysart.fi>
2022-03-01 18:53:43 +0200

    add login page infobox content

---
+++ frontend/tet/admin/src/components/login/InfoboxContent.sc.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const $InfoboxContent = styled.div`
+  white-space: pre-wrap;
+  & > ul: {
+    margin: 0;
+  }
+  & > li: {
+    text-indent: -5px;
+  }
+`;
