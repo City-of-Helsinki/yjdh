d07355afa9ceba3b03bbbdbc8e8f49f269cb2b3d
Ivan Melnik <melnik.job@gmail.com>
2022-03-01 10:43:09 +0300

    HL-440,441: Archive page for handler (#833)

--- frontend/shared/src/utils/menu.utils.ts
+++ frontend/shared/src/utils/menu.utils.ts
@@ -1,5 +1,13 @@
 import isServerSide from 'shared/server/is-server-side';
 
-export const isTabActive = (pathname: string): boolean => (
-  !isServerSide() && window.location.pathname.startsWith(pathname)
-);
+export const isTabActive = (pathname: string): boolean => {
+  if (!isServerSide()) {
+    if (window.location.pathname === '/' && pathname === '/') {
+      return true;
+    }
+    if (pathname !== '/') {
+      return window.location.pathname.startsWith(pathname);
+    }
+  }
+  return false;
+};
