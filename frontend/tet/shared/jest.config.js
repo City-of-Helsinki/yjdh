c8bf072d353c209e9a18f71c253c1e0f9ab308b1
Jaakko Nenonen <jaakko@codelicious.fi>
2022-03-04 20:00:28 +0200

    YJDH-434: rebase aftermath with tet/shared

--- frontend/tet/shared/jest.config.js
+++ frontend/tet/shared/jest.config.js
@@ -3,10 +3,11 @@ module.exports = {
   ...sharedConfig,
   globals: {
     'ts-jest': {
-      tsconfig: '<rootDir>/../../tsconfig.json',
+      tsconfig: '<rootDir>/tsconfig.json',
     },
   },
   moduleNameMapper: {
+    [`^shared\/(.*)$`]: '<rootDir>/../../shared/src/$1',
     ['^tet/shared/test/(.*)$']: '<rootDir>/test/$1',
     [`^tet/shared\/(.*)$`]: '<rootDir>src/$1',
   },
