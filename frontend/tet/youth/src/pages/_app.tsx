aabbc0c7902f4d36479a5bfb376b3a5fc5e3e042
Ville-Matias Pekkarinen <97732757+pekkvil@users.noreply.github.com>
2022-03-08 09:58:39 +0200

    TET-77 youth can filter postings list (#859)
    * Filter by date
    
    * User can remove filters
    
    * User can use quick search on front page
    
    * Add filter translations

--- frontend/tet/youth/src/pages/_app.tsx
+++ frontend/tet/youth/src/pages/_app.tsx
@@ -8,11 +8,21 @@ import { QueryClientProvider } from 'react-query';
 import BaseApp from 'shared/components/app/BaseApp';
 import Header from 'tet/youth/components/header/Header';
 import Footer from 'tet/youth/components/footer/Footer';
+import { linkedEventsUrl } from 'tet/youth/backend-api/backend-api';
+import BackendAPIProvider from 'shared/backend-api/BackendAPIProvider';
 
 const App: React.FC<AppProps> = (appProps) => (
-  <QueryClientProvider client={createQueryClient()}>
-    <BaseApp header={<Header />} footer={<Footer />} {...appProps} />
-  </QueryClientProvider>
+  <BackendAPIProvider
+    baseURL={linkedEventsUrl}
+    headers={{
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    }}
+  >
+    <QueryClientProvider client={createQueryClient()}>
+      <BaseApp header={<Header />} footer={<Footer />} {...appProps} />
+    </QueryClientProvider>
+  </BackendAPIProvider>
 );
 
 export default appWithTranslation(App);
--- frontend/tet/youth/src/pages/postings.tsx
+++ frontend/tet/youth/src/pages/postings.tsx
@@ -2,18 +2,10 @@ import type { NextPage } from 'next';
 import React from 'react';
 import { GetStaticProps } from 'next';
 import getServerSideTranslations from 'shared/i18n/get-server-side-translations';
-import Banner from 'tet/youth/components/banner/Banner';
-import QuickSearch from 'tet/youth/components/quickSearch/QuickSearch';
+import Postings from 'tet/youth/components/postings/Postings';
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Banner />
-      <QuickSearch />
-    </>
-  );
-};
+const PostingSearch: NextPage = () => <Postings />;
 
 export const getStaticProps: GetStaticProps = getServerSideTranslations('common');
 
-export default Home;
+export default PostingSearch;
