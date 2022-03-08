debbcd68b87955d30bc42eef0d8ebecdee1698ad
Kari Salminen <kari.salminen@anders.com>
2022-03-08 13:17:55 +0200

    KS-Backend: Add more youth summer voucher tests
    Add tests:
     - test_youth_application_activation_email_sending
     - test_youth_summer_voucher_email_language
     - test_youth_summer_voucher_email_sending
     - test_youth_summer_voucher_email_type
     - test_youth_summer_voucher_email_plaintext_html_similarity
     - test_youth_applications_accept_acceptable_with_invalid_smtp_server
     - test_youth_applications_handle_handled
       - test_youth_applications_accept_accepted merged into this
       - test_youth_applications_reject_rejected merged into this
    
    Add testing of the email sender and recipients for activation,
    processing and youth summer voucher emails.
    
    Add normalize_whitespace utility function and test for it.
    
    Refs YJDH-501

--- backend/kesaseteli/common/tests/test_utils.py
+++ backend/kesaseteli/common/tests/test_utils.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from common.utils import (
     has_whitespace,
     is_uppercase,
+    normalize_whitespace,
     validate_finnish_social_security_number,
     validate_optional_finnish_social_security_number,
 )
@@ -45,6 +46,23 @@ def test_has_whitespace(test_value, expected_result):
 )
 def test_is_uppercase(test_value, expected_result):
     assert is_uppercase(test_value) == expected_result
+
+
+@pytest.mark.parametrize(
+    "test_value,expected_result",
+    [
+        ("Test", "Test"),
+        ("  Test", "Test"),
+        ("Test  ", "Test"),
+        ("  Test    ", "Test"),
+        ("Te   st", "Te st"),
+        ("T  e s    t", "T e s t"),
+        ("   T     e  s  t    ", "T e s t"),
+        ("\n \tT\tes\nt  \ni\nn\t\n   g  ", "T es t i n g"),
+    ],
+)
+def test_normalize_whitespace(test_value, expected_result):
+    assert normalize_whitespace(test_value) == expected_result
 
 
 @pytest.mark.parametrize("test_value", get_empty_values())
