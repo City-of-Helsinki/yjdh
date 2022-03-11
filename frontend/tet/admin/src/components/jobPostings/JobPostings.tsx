d642f764782e06558c5a58ffa32b9a81f9a43d70
Ville-Matias Pekkarinen <ville-matias.pekkarinen@vincit.fi>
2022-03-11 15:19:22 +0200

    Accept terms before posting creation and final translations

--- frontend/tet/admin/src/components/jobPostings/JobPostings.tsx
+++ frontend/tet/admin/src/components/jobPostings/JobPostings.tsx
@@ -2,7 +2,6 @@ import { Button } from 'hds-react';
 import * as React from 'react';
 import Container from 'shared/components/container/Container';
 import { useTranslation } from 'next-i18next';
-
 import { $Heading, $HeadingContainer } from './JobPostings.sc';
 import { useQuery } from 'react-query';
 import { BackendEndpoint } from 'tet/admin/backend-api/backend-api';
@@ -13,19 +12,30 @@ import JobPostingsList from 'tet/admin/components/jobPostings/JobPostingsList';
 import { TetEvent, TetEvents } from 'tet-shared/types/linkedevents';
 import theme from 'shared/styles/theme';
 import { eventsToTetPostings } from 'tet-shared/backend-api/transformations';
+import useConfirm from 'shared/hooks/useConfirm';
 
 const JobPostings: React.FC = () => {
   const { t } = useTranslation();
   const { isLoading, data } = useQuery<TetEvents>(BackendEndpoint.TET_POSTINGS);
+  const { confirm } = useConfirm();
   const router = useRouter();
 
   if (isLoading) {
     return <PageLoadingSpinner />;
   }
 
-  console.dir(data);
-
   const postings = eventsToTetPostings(data);
+
+  const confirmTerms = async () => {
+    const isConfirmed = await confirm({
+      header: t('common:application.createTerms'),
+      submitButtonLabel: t('common:application.accept'),
+    });
+
+    if (isConfirmed) {
+      void router.push('/new');
+    }
+  };
 
   const content =
     postings.draft.length > 0 || postings.published.length > 0 ? (
@@ -41,7 +51,7 @@ const JobPostings: React.FC = () => {
       <$HeadingContainer>
         <$Heading>{t('common:application.jobPostings.title')}</$Heading>
       </$HeadingContainer>
-      <Button onClick={() => router.push('/new')}>{t('common:application.jobPostings.addNewPosting')}</Button>
+      <Button onClick={confirmTerms}>{t('common:application.jobPostings.addNewPosting')}</Button>
       {content}
     </Container>
   );
