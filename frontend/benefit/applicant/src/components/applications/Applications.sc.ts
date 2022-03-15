88c5a460cbe3c465a0f4bf3d6d266a4bb9cf83a4
ngoc <ngocdh236@gmail.com>
2022-03-15 16:21:46 +0200

    Fix translations, fix application step header layout

--- frontend/benefit/applicant/src/components/applications/Applications.sc.ts
+++ frontend/benefit/applicant/src/components/applications/Applications.sc.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const $PageHeader = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: start;
   margin-bottom: ${(props) => props.theme.spacing.m};
   margin-top: ${(props) => props.theme.spacing.xs};
   & > div {
