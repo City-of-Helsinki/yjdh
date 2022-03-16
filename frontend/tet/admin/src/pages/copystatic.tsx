76757d602253d22215e7a57a2512e8f4d4c9edf1
Ville-Matias Pekkarinen <97732757+pekkvil@users.noreply.github.com>
2022-03-16 11:41:40 +0200

    Load keywords and languageoptions in copy page (#903)

--- frontend/tet/admin/src/pages/copystatic.tsx
+++ frontend/tet/admin/src/pages/copystatic.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GetStaticProps, GetStaticPaths, NextPage } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 import getServerSideTranslations from 'shared/i18n/get-server-side-translations';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
@@ -8,24 +8,35 @@ import { BackendEndpoint } from 'tet/admin/backend-api/backend-api';
 import { TetEvent } from 'tet-shared/types/linkedevents';
 import PageLoadingSpinner from 'shared/components/pages/PageLoadingSpinner';
 import { eventToTetPosting } from 'tet-shared/backend-api/transformations';
+import useKeywordType from 'tet-shared/hooks/backend/useKeywordType';
 import { useQuery } from 'react-query';
 import PageNotFound from 'shared/components/pages/PageNotFound';
 import withAuth from 'shared/components/hocs/withAuth';
+import useLanguageOptions from 'tet-shared/hooks/translation/useLanguageOptions';
+import EditorLoadingError from 'tet/admin/components/editor/EditorLoadingError';
 
 const CopyStaticPage: NextPage = () => {
   const { t } = useTranslation();
   const router = useRouter();
   const id = router.query.id as string;
-  const { isLoading, data } = useQuery<TetEvent>(`${BackendEndpoint.TET_POSTINGS}${id}`);
+  const { isLoading, data, error } = useQuery<TetEvent>(`${BackendEndpoint.TET_POSTINGS}${id}`);
 
-  if (isLoading) {
+  const keywordResult = useKeywordType();
+  const languageOptions = useLanguageOptions();
+
+  if (isLoading || keywordResult.isLoading) {
     return <PageLoadingSpinner />;
   }
 
+  if (error || keywordResult.error) {
+    const err = (error as Error) || keywordResult.error;
+    return <EditorLoadingError error={err} />;
+  }
+
   if (data) {
-    const postingData = { ...eventToTetPosting(data) };
-    delete postingData.id;
-    return <EditById title={t('common:editor.copyTitle')} data={postingData} allowDelete={false} />;
+    const posting = eventToTetPosting(data, keywordResult.getKeywordType, languageOptions);
+    delete posting.id;
+    return <EditById title={t('common:editor.copyTitle')} data={posting} allowDelete={false} />;
   } else {
     return <PageNotFound />;
   }
