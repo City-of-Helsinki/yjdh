1e8f1fed3a9da1e0ce5992a2d9bfb74013d2782c
Jaakko Nenonen <jaakko@codelicious.fi>
2022-04-05 09:58:24 +0300

    revert attachment change

--- frontend/shared/src/utils/attachment.utils.ts
+++ frontend/shared/src/utils/attachment.utils.ts
@@ -1,46 +1,22 @@
-import Attachment, {
-  BenefitAttachment,
-  KesaseteliAttachment,
-} from '../types/attachment';
-
-export const isKesaseteliAttachment = (
-  attachment: unknown
-): attachment is KesaseteliAttachment =>
-  Boolean(
-    attachment &&
-      typeof attachment === 'object' &&
-      'summer_voucher' in attachment
-  );
-
-export const isBenefitAttachment = (
-  attachment: unknown
-): attachment is BenefitAttachment =>
-  Boolean(
-    attachment &&
-      typeof attachment === 'object' &&
-      !('summer_voucher' in attachment)
-  );
+import Attachment from '../types/attachment';
 
 export const getAttachmentsByType = <T extends Attachment>(
   attachments: T[],
   type: string
 ): T[] =>
-  attachments?.filter(
-    (attachment) =>
-      (isKesaseteliAttachment(attachment) &&
-        attachment.attachment_type === type) ||
-      (isBenefitAttachment(attachment) && attachment.attachmentType === type)
+  attachments?.filter((attachment) =>
+    'attachment_type' in attachment
+      ? attachment.attachment_type === type
+      : attachment.attachmentType === type
   ) ?? [];
 
 export const getAttachmentsSummary = <T extends Attachment>(
   attachments?: T[]
 ): string =>
   attachments
-    ?.map(
-      (attachment) =>
-        (isKesaseteliAttachment(attachment) &&
-          attachment.attachment_file_name) ||
-        (isBenefitAttachment(attachment) && attachment.attachmentFileName) ||
-        undefined
+    ?.map((attachment) =>
+      'attachment_file_name' in attachment
+        ? attachment.attachment_file_name
+        : attachment.attachmentFileName
     )
     .join(', ') ?? '-';
