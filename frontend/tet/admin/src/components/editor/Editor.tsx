9953b695a4aaeb1cf42a92bfbbd2cb9b2c1b8f78
Ville-Matias Pekkarinen <97732757+pekkvil@users.noreply.github.com>
2022-06-08 09:50:18 +0300

    TETP-203 accessibility fixes (#1154)
    * Edit backbutton
    
    * Add aria-label translations

--- frontend/tet/admin/src/components/editor/Editor.tsx
+++ frontend/tet/admin/src/components/editor/Editor.tsx
@@ -38,7 +38,7 @@ const Editor: React.FC<EditorProps> = ({ initialValue }) => {
   return (
     <>
       <FormProvider {...methods}>
-        <form aria-label="add/modify tet posting">
+        <form aria-label={t('common:editor.formLabel')}>
           <HiddenIdInput id="id" initialValue={initialValue?.id} />
           <EditorErrorNotification />
           <p>* {t('common:editor.requiredInfo')}</p>
