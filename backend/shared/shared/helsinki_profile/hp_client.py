1b7a14954edb8dbce17a87e3c27a49c762ccfc6f
Matti Korttila <matti.korttila@sysart.fi>
2022-06-15 15:19:02 +0300

    fix OIDC setup, add HelsinkiProfileClient

---
+++ backend/shared/shared/helsinki_profile/hp_client.py
@@ -0,0 +1,10 @@
+
+
+class HelsinkiProfileClient:
+    def __init__(self):
+        pass
+
+    def get_profile(self, request):
+        return {
+            "user_ssn": "100771-999M"  # TODO Testitunnistautuja
+        }
