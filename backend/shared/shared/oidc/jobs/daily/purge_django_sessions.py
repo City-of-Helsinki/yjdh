95cd4e2140c8e0077b422604a99cf56aabb9b45f
Mikko Vihonen <mikko.vihonen@nitor.com>
2022-06-27 13:23:39 +0300

    FIX move stale session data purging under shared.oidc so that it will be available to all YJDH services

--- backend/shared/shared/oidc/jobs/daily/purge_django_sessions.py
+++ backend/shared/shared/oidc/jobs/daily/purge_django_sessions.py
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 from django.core import management
 from django_extensions.management.jobs import DailyJob
+=======
+from django_extensions.management.jobs import DailyJob
+from django.core import management
+
+from applications.models import Attachment
+>>>>>>> ebfd08e2 (FIX move stale session data purging under shared.oidc so that it will be available to all YJDH services)
 
 
 class Job(DailyJob):
