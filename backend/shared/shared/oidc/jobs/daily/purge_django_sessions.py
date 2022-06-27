ba163fa7333e448840efd32389a585d3da87a14e
Mikko Vihonen <mikko.vihonen@nitor.com>
2022-06-27 19:33:50 +0300

    FIX merge conflict

--- backend/shared/shared/oidc/jobs/daily/purge_django_sessions.py
+++ backend/shared/shared/oidc/jobs/daily/purge_django_sessions.py
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from django.core import management
 from django_extensions.management.jobs import DailyJob
-=======
-from django_extensions.management.jobs import DailyJob
-from django.core import management
 
-<<<<<<< HEAD
-from applications.models import Attachment
->>>>>>> ebfd08e2 (FIX move stale session data purging under shared.oidc so that it will be available to all YJDH services)
-
-=======
->>>>>>> 78c324b9 (FIX remove unused imports)
-=======
-from django_extensions.management.jobs import DailyJob
-from django.core import management
-
->>>>>>> 748ab5133845f50888ec860d7a1392382c6b4106
 
 class Job(DailyJob):
     help = "Purge expired Django sessions by running clearsessions management command."
