9c41096ff5d11f88e984fe0b0f62243f76d40787
Jaakko Nenonen <jaakko@codelicious.fi>
2022-08-25 20:26:33 +0300

    YJDH-608: fix validation issues on form on both text inputs and date inputs

--- frontend/kesaseteli/employer/src/hooks/application/useApplicationFormField.ts
+++ frontend/kesaseteli/employer/src/hooks/application/useApplicationFormField.ts
@@ -1,7 +1,6 @@
 import useGetApplicationFormFieldLabel from 'kesaseteli/employer/hooks/application/useGetApplicationFormFieldLabel';
 import ApplicationFieldPath from 'kesaseteli/employer/types/application-field-path';
 import { useTranslation } from 'next-i18next';
-import React from 'react';
 import {
   Control,
   ErrorOption,
@@ -60,81 +59,52 @@ const useApplicationFormField = <V extends Value>(
     '') as ApplicationFieldName;
   const defaultLabel = useGetApplicationFormFieldLabel(fieldName);
 
-  const getValue = React.useCallback((): V => {
-    const value = getValues(id) as string;
-    if (isDateObject(parseDate(value))) {
-      return convertToUIDateFormat(value) as V;
-    }
-    return value as V;
-  }, [getValues, id]);
-
-  const setValueF = React.useCallback(
-    (value: V) => setValue(id, value),
-    [setValue, id]
-  );
-  const watchF = React.useCallback(() => watch(id) as V, [watch, id]);
-  const getError = React.useCallback(
-    (): FieldError | undefined =>
-      get(formState.errors, id) as FieldError | undefined,
-    [formState, id]
-  );
-  const hasError = React.useCallback(() => Boolean(getError()), [getError]);
-
-  const setError = React.useCallback(
-    (error: ErrorOption) => setErrorF(id, error),
-    [id, setErrorF]
-  );
-
-  const getErrorText = React.useCallback((): string | undefined => {
-    const error = getError();
-    if (!error) {
-      return undefined;
-    }
-    const { message } = error;
-    if (message) {
-      return message;
-    }
-    switch (fieldName) {
-      case 'target_group':
-      case 'hired_without_voucher_assessment':
-        return t(`common:application.form.errors.selectionGroups`);
-
-      case 'employment_contract':
-      case 'payslip':
-        return t(`common:application.form.errors.attachments`);
-
-      default:
-        return t(`common:application.form.errors.${error.type}`);
-    }
-  }, [fieldName, getError, t]);
-
-  const clearValue = React.useCallback(() => setValue(id, ''), [setValue, id]);
-  const triggerF = React.useCallback(
-    () => trigger(id, { shouldFocus: true }),
-    [trigger, id]
-  );
-  const clearErrorsF = React.useCallback(
-    () => clearErrors(id),
-    [clearErrors, id]
-  );
-  const setFocusF = React.useCallback(() => setFocus(id), [setFocus, id]);
+  const getError = (): FieldError | undefined =>
+    get(formState.errors, id) as FieldError | undefined;
 
   return {
     control,
     register,
     fieldName,
     defaultLabel,
-    getValue,
-    setValue: setValueF,
-    watch: watchF,
+    getValue: (): V => {
+      const value = getValues(id) as string;
+      if (isDateObject(parseDate(value))) {
+        return convertToUIDateFormat(value) as V;
+      }
+      return value as V;
+    },
+    setValue: (value: V) => setValue(id, value),
+    watch: () => watch(id) as V,
     getError,
-    hasError,
-    getErrorText,
-    setError,
-    clearValue,
-    trigger: triggerF,
-    clearErrors: clearErrorsF,
-    setFocus: setFocusF,
+    hasError: () => Boolean(getError()),
+    getErrorText: (): string | undefined => {
+      const error = getError();
+      if (!error) {
+        return undefined;
+      }
+      const { message } = error;
+      if (message) {
+        return message;
+      }
+      switch (fieldName) {
+        case 'target_group':
+        case 'hired_without_voucher_assessment':
+          return t(`common:application.form.errors.selectionGroups`);
+
+        case 'employment_contract':
+        case 'payslip':
+          return t(`common:application.form.errors.attachments`);
+
+        default:
+          return t(`common:application.form.errors.${error.type}`);
+      }
+    },
+    setError: (error: ErrorOption) => setErrorF(id, error),
+    clearValue: () => setValue(id, ''),
+    trigger: () => trigger(id, { shouldFocus: true }),
+    clearErrors: () => clearErrors(id),
+    setFocus: () => setFocus(id),
   };
 };
 export default useApplicationFormField;
