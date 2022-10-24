cd45ef4908345e7916c4debd22150f6b0c8d5455
Kari Salminen <kari.salminen@anders.com>
2022-10-24 13:35:27 +0300

    HL-Backend: Use mock user name for /v1/users/me/ with mock flag
    Endpoint /v1/users/me/:
     - If using NEXT_PUBLIC_MOCK_FLAG and an anonymous user then return
       user with name Test User
    
    Refs HL-566

--- backend/benefit/users/api/v1/views.py
+++ backend/benefit/users/api/v1/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import PermissionDenied
 from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
@@ -6,6 +7,7 @@ from rest_framework.views import APIView
 
 from common.permissions import BFIsAuthenticated
 from users.api.v1.serializers import UserSerializer
+from users.utils import set_mock_user_name
 
 
 @extend_schema(
@@ -26,4 +28,6 @@ class CurrentUserView(APIView):
     def _get_current_user(self, request):
         if not request.user.is_authenticated and not settings.NEXT_PUBLIC_MOCK_FLAG:
             raise PermissionDenied
+        if settings.NEXT_PUBLIC_MOCK_FLAG and isinstance(request.user, AnonymousUser):
+            set_mock_user_name(request.user)
         return request.user
