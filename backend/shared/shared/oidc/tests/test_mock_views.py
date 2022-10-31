99f87340ea58df782eb270d52c8bb2f409f07238
Kari Salminen <kari.salminen@anders.com>
2022-10-31 13:27:03 +0200

    Shared backend: Add is_staff & is_superuser support to OIDC mock user
    Add settings to control OIDC mock user's is_staff & is_superuser values:
     - settings.OIDC_MOCK_USER_IS_STAFF -> OIDC mock user's is_staff
       - If the setting is not set at all then will default to False
     - settings.OIDC_MOCK_USER_IS_SUPERUSER -> OIDC mock user's is_superuser
       - If the setting is not set at all then will default to False
    
    The OIDC mock user is created by MockAuthenticationRequestView which is
    mapped to oidc's authenticate endpoint
    (See backend/shared/shared/oidc/urls.py) if NEXT_PUBLIC_MOCK_FLAG is set
    
    Refs HL-566

--- backend/shared/shared/oidc/tests/test_mock_views.py
+++ backend/shared/shared/oidc/tests/test_mock_views.py
@@ -1,5 +1,8 @@
+import itertools
+
 import pytest
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import override_settings
 from django.urls import path, reverse
 
@@ -35,17 +38,53 @@ urlpatterns = [
 
 
 @pytest.mark.django_db
+@pytest.mark.parametrize(
+    "oidc_mock_user_is_staff,oidc_mock_user_is_superuser",
+    itertools.product([None, False, True], repeat=2),  # None means no setting at all
+)
 @override_settings(
     NEXT_PUBLIC_MOCK_FLAG=True,
     LOGIN_REDIRECT_URL="http://example.com/login/",
     ROOT_URLCONF=__name__,
 )
-def test_login_view(client):
+def test_login_view(
+    settings, client, oidc_mock_user_is_staff, oidc_mock_user_is_superuser
+):
+    if oidc_mock_user_is_staff is None:
+        if hasattr(settings, "OIDC_MOCK_USER_IS_STAFF"):
+            delattr(settings, "OIDC_MOCK_USER_IS_STAFF")
+    else:
+        settings.OIDC_MOCK_USER_IS_STAFF = oidc_mock_user_is_staff
+
+    if oidc_mock_user_is_superuser is None:
+        if hasattr(settings, "OIDC_MOCK_USER_IS_SUPERUSER"):
+            delattr(settings, "OIDC_MOCK_USER_IS_SUPERUSER")
+    else:
+        settings.OIDC_MOCK_USER_IS_SUPERUSER = oidc_mock_user_is_superuser
+
+    assert (oidc_mock_user_is_staff is not None) == hasattr(
+        settings, "OIDC_MOCK_USER_IS_STAFF"
+    )
+    assert (oidc_mock_user_is_superuser is not None) == hasattr(
+        settings, "OIDC_MOCK_USER_IS_SUPERUSER"
+    )
+
+    assert bool(oidc_mock_user_is_staff) == getattr(
+        settings, "OIDC_MOCK_USER_IS_STAFF", False
+    )
+    assert bool(oidc_mock_user_is_superuser) == getattr(
+        settings, "OIDC_MOCK_USER_IS_SUPERUSER", False
+    )
+
     login_url = reverse("oidc_authentication_init")
     response = client.get(login_url)
 
     assert response.url == settings.LOGIN_REDIRECT_URL
     assert "_auth_user_id" in client.session  # User authenticated
+    user = get_user_model().objects.get(pk=client.session.get("_auth_user_id"))
+    assert user.is_authenticated
+    assert user.is_staff == bool(oidc_mock_user_is_staff)
+    assert user.is_superuser == bool(oidc_mock_user_is_superuser)
 
 
 @pytest.mark.django_db
