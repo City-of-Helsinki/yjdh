f976cb02564cb755ca237ce47ddf4855ee26b2b5
Kari Salminen <kari.salminen@anders.com>
2022-11-30 09:24:06 +0200

    TET-Backend: Remove obsolete employer frontend
    Also:
     - Replace uses of EMPLOYER_URL with ADMIN_URL as the login buttons
       for both OIDC and ADFS logins are on ADMIN_URL's front page.
     - Add missing yarn commands related to docker-compose.tet.yml which
       builds both tet-admin and tet-youth frontends:
       - tet
       - tet:up
       - tet:build
       - tet:rebuild
    
    Refs None

--- frontend/tet/employer/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-const { join } = require('path');
-
-module.exports = {
-  extends: ['../../.eslintrc.nextjs.js'],
-  rules: {
-    'import/no-extraneous-dependencies': ['error', { packageDir: [__dirname, join(__dirname, '../../')] }],
-  },
-};
