d8a456b1b7b59bcabcbf1cd65736fb667e9a6b7c
Riku Kestilä <riku.kestila@vincit.fi>
2023-01-13 15:49:04 +0200

    Add command for seeding applications data

---
+++ backend/benefit/applications/management/commands/seed.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+from applications.models import Application, ApplicationBasis
+from applications.tests.factories import ApplicationFactory
+from applications.enums import ApplicationStatus
+import os
+
+
+class Command(BaseCommand):
+    help = "Seed the database with applications"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number",
+            type=int,
+            default=10,
+            help="Number of applications to create",
+        )
+
+    def handle(self, *args, **options):
+        total_created = len(ApplicationStatus.values) * options["number"]
+        if os.getenv("DEBUG") != "1":
+            self.stdout.write("Seeding is only allowed in debug mode")
+            return
+        run_seed(options["number"])
+        self.stdout.write(f"Created {total_created} applications")
+
+
+def clear_applications():
+    """Clear all seeded applications and application basis records,
+    which are not deleted with on_delete=CASCADE"""
+    Application.objects.all().delete()
+    ApplicationBasis.objects.all().delete()
+
+
+def create_application(status):
+    """Create an application"""
+    application = ApplicationFactory()
+    application.status = status
+    application.save()
+
+
+def run_seed(number):
+    """Delete all existing applications and create applications for all statuses"""
+    clear_applications()
+
+    for i in range(number):
+        for status in ApplicationStatus.values:
+            create_application(status)
---
+++ backend/benefit/applications/tests/test_application_tasks.py
@@ -0,0 +1,18 @@
+from io import StringIO
+from django.core.management import call_command
+from applications.models import Application
+from applications.enums import ApplicationStatus
+
+
+def test_seed_applications_with_arguments():
+    amount = 10
+    statuses = ApplicationStatus.values
+    total_created = len(ApplicationStatus.values) * amount
+    out = StringIO()
+    call_command("seed", number=amount, stdout=out)
+
+    seeded_applications = Application.objects.filter(
+        status__in=statuses,
+    )
+    assert seeded_applications.count() == total_created
+    assert f"Created {total_created} applications" in out.getvalue()
