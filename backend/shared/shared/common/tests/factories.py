50693c06f2be86e2ea00aa2bcf7425f51d69766d
Maks Turtiainen <maks.turtiainen@vincit.fi>
2023-02-02 20:03:01 +0200

    Fix typo

--- backend/shared/shared/common/tests/factories.py
+++ backend/shared/shared/common/tests/factories.py
@@ -9,7 +9,7 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
-    username = factory.Faker("username")
+    username = factory.Faker("user_name")
     email = factory.Faker("email")
 
     class Meta:
