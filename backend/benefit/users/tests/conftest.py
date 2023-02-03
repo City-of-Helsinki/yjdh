59579e534970b3a7b08b58c8c38b23fa59f764c9
Maks Turtiainen <maks.turtiainen@vincit.fi>
2023-02-03 02:00:42 +0200

    GDPR api tests with authentication

--- backend/benefit/users/tests/conftest.py
+++ backend/benefit/users/tests/conftest.py
@@ -1,14 +1,21 @@
 import pytest
+from rest_framework.test import APIClient
 
 from common.tests.conftest import *  # noqa
 from helsinkibenefit.tests.conftest import *  # noqa
+from shared.common.tests.factories import HandlerUserFactory, HelsinkiProfileUserFactory
 
-from .factories import UserFactory
+
+@pytest.fixture
+def gdpr_api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def user():
+    return HelsinkiProfileUserFactory()
 
 
 @pytest.fixture
 def admin_user():
-    user = UserFactory()
-    user.is_staff = True
-    user.save()
-    return user
+    return HandlerUserFactory()
--- backend/benefit/users/tests/test_gdpr_api.py
+++ backend/benefit/users/tests/test_gdpr_api.py
@@ -1,13 +1,17 @@
+import datetime
 import json
 from typing import Optional
 
 import pytest
+from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import override_settings
 from django.urls import reverse
+from helusers.settings import api_token_auth_settings
+from jose import jwt
 
-from shared.common.tests.factories import HelsinkiProfileUserFactory
 from users.models import format_date
+
+from .keys import rsa_key
 
 User = get_user_model()
 
@@ -18,11 +22,51 @@ def extract_value(gdpr_api_response: dict, key: str) -> Optional[str]:
     return next((item["value"] for item in user if item["key"] == key), None)
 
 
-@override_settings(NEXT_PUBLIC_MOCK_FLAG=True)
-def test_get_profile_data_from_gdpr_api_no_auth(api_client):
-    """Test that the profile data is valid without authentication."""
-    user = HelsinkiProfileUserFactory()
-    response = api_client.get(reverse("gdpr_v1", kwargs={"uuid": user.username}))
+def get_api_token_for_user_with_scopes(user, scopes: list, requests_mock):
+    """Build a proper auth token with desired scopes."""
+    audience = api_token_auth_settings.AUDIENCE
+    issuer = api_token_auth_settings.ISSUER
+    auth_field = api_token_auth_settings.API_AUTHORIZATION_FIELD
+    config_url = f"{issuer}/.well-known/openid-configuration"
+    jwks_url = f"{issuer}/jwks"
+
+    configuration = {
+        "issuer": issuer,
+        "jwks_uri": jwks_url,
+    }
+
+    keys = {"keys": [rsa_key.public_key_jwk]}
+
+    now = datetime.datetime.now()
+    expire = now + datetime.timedelta(days=14)
+
+    jwt_data = {
+        "iss": issuer,
+        "sub": str(user.username),
+        "aud": audience,
+        "exp": int(expire.timestamp()),
+        "iat": int(now.timestamp()),
+        auth_field: scopes,
+    }
+    encoded_jwt = jwt.encode(
+        jwt_data, key=rsa_key.private_key_pem, algorithm=rsa_key.jose_algorithm
+    )
+
+    requests_mock.get(config_url, json=configuration)
+    requests_mock.get(jwks_url, json=keys)
+
+    auth_header = f"{api_token_auth_settings.AUTH_SCHEME} {encoded_jwt}"
+
+    return auth_header
+
+
+def test_get_profile_data_from_gdpr_api(gdpr_api_client, user, requests_mock):
+    """Test that the profile data is valid."""
+    auth_header = get_api_token_for_user_with_scopes(
+        user, [settings.GDPR_API_QUERY_SCOPE], requests_mock
+    )
+    gdpr_api_client.credentials(HTTP_AUTHORIZATION=auth_header)
+    response = gdpr_api_client.get(reverse("gdpr_v1", kwargs={"uuid": user.username}))
     response_dict = json.loads(response.content)
     assert response.status_code == 200
     assert extract_value(response_dict, "FIRST_NAME") == user.first_name
@@ -36,11 +80,13 @@ def test_get_profile_data_from_gdpr_api_no_auth(api_client):
     )
 
 
-@override_settings(NEXT_PUBLIC_MOCK_FLAG=True)
-def test_delete_profile_data_from_gdpr_api_no_auth(api_client):
+def test_delete_profile_data_from_gdpr_api(gdpr_api_client, user, requests_mock):
     """Test that the profile data is deleted without authentication."""
-    user = HelsinkiProfileUserFactory()
-    response = api_client.delete(reverse("gdpr_v1", kwargs={"uuid": user.username}))
+    auth_header = get_api_token_for_user_with_scopes(
+        user, [settings.GDPR_API_DELETE_SCOPE], requests_mock
+    )
+    gdpr_api_client.credentials(HTTP_AUTHORIZATION=auth_header)
+    response = gdpr_api_client.delete(reverse("gdpr_v1", kwargs={"uuid": user.username}))
     assert response.status_code == 204
     with pytest.raises(User.DoesNotExist):
         User.objects.get(username=user.username)
