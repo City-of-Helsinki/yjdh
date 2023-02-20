50dd67c7bb12ed42f665e0d789ccab12fdc2c39c
Riku Kestilä <riku.kestila@vincit.fi>
2023-02-20 11:43:30 +0200

    Fix code style issue

--- backend/benefit/helsinkibenefit/settings.py
+++ backend/benefit/helsinkibenefit/settings.py
@@ -464,11 +464,12 @@ if os.path.exists(local_settings_path):
     exec(code, globals(), locals())
 
 # S3 settings
+
 USE_S3 = env("USE_S3")
 
 if USE_S3:
-    AWS_S3_ENDPOINT_URL=env("S3_ENDPOINT_URL")
-    DEFAULT_FILE_STORAGE="storages.backends.s3boto3.S3Boto3Storage"
+    AWS_S3_ENDPOINT_URL = env("S3_ENDPOINT_URL")
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     AWS_ACCESS_KEY_ID = env("S3_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = env("S3_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = env("S3_STORAGE_BUCKET_NAME")
