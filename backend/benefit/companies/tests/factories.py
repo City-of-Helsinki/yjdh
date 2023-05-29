d9a5a9c48aa0b6062c063adca5dfc536de0990a4
Sampo Tawast <sampo.tawast@gmail.com>
2023-05-29 09:55:13 +0300

    Use Finnish locale in factories

--- backend/benefit/companies/tests/factories.py
+++ backend/benefit/companies/tests/factories.py
@@ -5,15 +5,15 @@ from shared.service_bus.enums import YtjOrganizationCode
 
 
 class CompanyFactory(factory.django.DjangoModelFactory):
-    name = factory.Faker("company")
+    name = factory.Faker("company", locale="fi_FI")
     business_id = factory.Faker("numerify", text="#######-#")
     company_form = "oy"
     company_form_code = YtjOrganizationCode.COMPANY_FORM_CODE_DEFAULT
     bank_account_number = factory.Faker("iban", locale="fi_FI")
 
-    street_address = factory.Faker("street_address")
-    postcode = factory.Faker("postcode")
-    city = factory.Faker("city")
+    street_address = factory.Faker("street_address", locale="fi_FI")
+    postcode = factory.Faker("postcode", locale="fi_FI")
+    city = factory.Faker("city", locale="fi_FI")
 
     class Meta:
         model = Company
