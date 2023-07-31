da1bbf5f893d9394fbc0a075d8f8965b2e4b9cda
Sampo Tawast <sampo.tawast@gmail.com>
2023-07-31 14:14:13 +0300

    feat: add applicant cookie banner

--- frontend/benefit/applicant/src/components/layout/Layout.tsx
+++ frontend/benefit/applicant/src/components/layout/Layout.tsx
@@ -1,6 +1,8 @@
 import Header from 'benefit/applicant/components/header/Header';
 import TermsOfService from 'benefit/applicant/components/termsOfService/TermsOfService';
 import { IS_CLIENT, LOCAL_STORAGE_KEYS } from 'benefit/applicant/constants';
+import { useTranslation } from 'benefit/applicant/i18n';
+import { CookieModal } from 'hds-react';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import * as React from 'react';
@@ -8,6 +10,11 @@ import useAuth from 'shared/hooks/useAuth';
 
 import { ROUTES } from '../../constants';
 import { $Main } from './Layout.sc';
+
+type SupportedLanguage = 'fi' | 'en' | 'sv';
+type ConsentObject = {
+  [x: string]: boolean;
+};
 
 const Footer = dynamic(
   () => import('benefit/applicant/components/footer/Footer'),
@@ -18,6 +25,8 @@ type Props = { children: React.ReactNode };
 
 const Layout: React.FC<Props> = ({ children, ...rest }) => {
   const { isAuthenticated } = useAuth();
+  const { t, i18n } = useTranslation();
+
   const [isTermsOfServiceApproved, setIsTermsOfSerivceApproved] =
     React.useState(false);
   const router = useRouter();
@@ -34,6 +43,32 @@ const Layout: React.FC<Props> = ({ children, ...rest }) => {
     }
   }, []);
 
+  const [language, setLanguage] = React.useState<SupportedLanguage>(
+    (i18n.language as SupportedLanguage) ?? 'fi'
+  );
+  const onLanguageChange = (newLang: SupportedLanguage): void =>
+    setLanguage(newLang);
+  const contentSource = {
+    siteName: t('common:appName'),
+    currentLanguage: language,
+    optionalCookies: {
+      cookies: [
+        {
+          commonGroup: 'statistics',
+          commonCookie: 'matomo',
+        },
+      ],
+    },
+    language: {
+      onLanguageChange,
+    },
+    focusTargetSelector: 'main',
+    onAllConsentsGiven: (consents: ConsentObject) => {
+      // TODO: Set tracking on/off based on each consent
+      // eslint-disable-next-line no-console
+      console.log(consents);
+    },
+  };
   return (
     <$Main backgroundColor={bgColor} {...rest}>
       <Header />
@@ -45,6 +80,7 @@ const Layout: React.FC<Props> = ({ children, ...rest }) => {
         children
       )}
       <Footer />
+      <CookieModal contentSource={contentSource} />;
     </$Main>
   );
 };
