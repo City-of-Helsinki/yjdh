de401d71e14657c48dcc746691235f239a2282a3
Sampo Tawast <sampo.tawast@gmail.com>
2023-12-20 15:13:15 +0200

    fix: further change gdpr ssn mask

--- frontend/shared/src/utils/mask-gdpr-data.ts
+++ frontend/shared/src/utils/mask-gdpr-data.ts
@@ -6,7 +6,9 @@ import { isString } from 'shared/utils/type-guards';
  */
 
 const maskFinnishSsn = (text: unknown): unknown =>
-  isString(text) ? text.replace(/\d{6}.\d{3}[\dA-z]/g, '*'.repeat(11)) : text;
+  isString(text)
+    ? text.replace(/\d{6}[+-y]\d{3}[\dA-z]/g, '*'.repeat(11))
+    : text;
 
 /* Gets the length of the given variable */
 const getLengthOfAttribute = (attrValue: unknown): number =>
