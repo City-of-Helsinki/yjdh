f35eb714246b7e6b545d7cd353afc5921b3c8e9c
Riku Kestilä <riku.kestila@vincit.fi>
2024-01-09 11:36:41 +0200

    fix: conflicting migrations

--- backend/benefit/applications/migrations/0049_add_rejected_by_talpa_status.py
+++ backend/benefit/applications/migrations/0049_add_rejected_by_talpa_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('applications', '0047_remove_phone_number_restriction'),
+        ('applications', '0048_alter_attachment_attachment_type'),
     ]
 
     operations = [
--- backend/benefit/applications/migrations/0050_alter_applicationbatch_status.py
+++ backend/benefit/applications/migrations/0050_alter_applicationbatch_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('applications', '0048_add_rejected_by_talpa_status'),
+        ('applications', '0049_add_rejected_by_talpa_status'),
     ]
 
     operations = [
