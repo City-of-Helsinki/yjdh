19ea28968835f52db29d3f5753367e5339e30248
Niko Lindroos <niko.j.makela@gmail.com>
2024-03-12 17:19:59 +0200

    test: use swc-jest to improve performance
    YJDH-690.
    
    Installation guide: https://swc.rs/docs/usage/jest#usage.
    Public beanchmarks: https://swc.rs/docs/benchmarks.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
   testEnvironment: 'jsdom',
   collectCoverageFrom: ['./**/*.{ts,tsx,js,jsx}'],
   modulePathIgnorePatterns: ['.next'],
-  testPathIgnorePatterns: ['__tests__/utils','__tests__/types', 'component-apis'],
+  testPathIgnorePatterns: ['__tests__/utils', '__tests__/types', 'component-apis'],
 };
