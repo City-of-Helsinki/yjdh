20feef4384d25dfeb75e15910d10b068edde8ed9
Sampo Tawast <sampo.tawast@gmail.com>
2024-04-15 13:58:08 +0300

    refactor(shared): add possibility to scroll into any element by using selector

--- frontend/shared/src/utils/dom.utils.ts
+++ frontend/shared/src/utils/dom.utils.ts
@@ -1,7 +1,4 @@
-export const focusAndScroll = (elementId: string | undefined): void => {
-  // eslint-disable-next-line unicorn/prefer-query-selector
-  const element = elementId ? document.getElementById(elementId) : null;
-
+const handleScroll = (element?: HTMLElement): void => {
   if (!element) {
     return window.scrollTo({
       top: 0,
@@ -21,4 +18,25 @@ export const focusAndScroll = (elementId: string | undefined): void => {
     top: offsetPosition,
     behavior: 'smooth',
   });
+};
+
+/**
+ * Focus and scroll to element using it's id
+ * @param elementId
+ * @returns void
+ */
+export const focusAndScroll = (elementId: string): void => {
+  // eslint-disable-next-line unicorn/prefer-query-selector
+  const element = document.getElementById(elementId);
+  return handleScroll(element);
+};
+
+/**
+ * Focus and scroll to any element using querySelector
+ * @param elementSelector
+ * @returns void
+ */
+export const focusAndScrollTo = (elementSelector: string): void => {
+  const element = document.querySelector(elementSelector);
+  return handleScroll(element as HTMLElement);
 };
