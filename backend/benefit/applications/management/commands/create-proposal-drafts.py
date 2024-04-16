4eda910800648ce761349e2d67b65a4fce37958a
Sampo Tawast <5328394+sirtawast@users.noreply.github.com>
2024-04-16 15:56:11 +0300

    chore: add a migration command to create needed proposal drafts (#2931)

---
+++ backend/benefit/applications/management/commands/create-proposal-drafts.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+from applications.models import AhjoDecisionProposalDraft, Application
+
+
+class Command(BaseCommand):
+    help = "Create decision proposal drafts for older than 2024-04-16 applications"
+
+    def handle(self, *args, **options):
+        applications = Application.objects.all()
+        total_created = 0
+        for application in applications:
+            try:
+                application.decision_proposal_draft
+            except:  # noqa
+                AhjoDecisionProposalDraft.objects.create(
+                    application=application,
+                )
+                total_created += 1
+        self.stdout.write(f"Created {total_created} decision proposal drafts")
