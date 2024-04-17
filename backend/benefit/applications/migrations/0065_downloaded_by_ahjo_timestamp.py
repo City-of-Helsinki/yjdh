dbc38d97b374a8f56958a2deb903b0eda8590658
Riku Kestilä <riku.kestila@vincit.fi>
2024-04-17 10:04:31 +0300

    fix: conflicting migration

--- backend/benefit/applications/migrations/0065_downloaded_by_ahjo_timestamp.py
+++ backend/benefit/applications/migrations/0065_downloaded_by_ahjo_timestamp.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('applications', '0062_alter_applicationalteration_recovery_amount'),
+        ('applications', '0064_decision_proposal_drafts'),
     ]
 
     operations = [
