1a877e2105668645b19de5ea3dd73ce197776dc8
Riku Kestilä <riku.kestila@vincit.fi>
2024-05-28 11:06:06 +0300

    feat: run ahjo decision details requests hourly

--- backend/benefit/applications/jobs/hourly/hourly_ahjo_job.py
+++ backend/benefit/applications/jobs/hourly/hourly_ahjo_job.py
@@ -1,13 +1,22 @@
+from django.conf import settings
 from django.core.management import call_command
 from django_extensions.management.jobs import HourlyJob
 
+from applications.enums import AhjoRequestType
+
 """
 A hourly job to refresh the Ahjo access token via the refresh token saved in the Django database.
+If the Ahjo automation is enabled, the job will also send requests to Ahjo to get decision details.
 """
 
 
 class Job(HourlyJob):
-    help = "Hourly Ahjo refresh jobs are executed here."
+    help = "Hourly Ahjo jobs are executed here."
 
     def execute(self):
         call_command("refresh_ahjo_token")
+
+        if settings.ENABLE_AHJO_AUTOMATION:
+            call_command(
+                "send_ahjo_requests", request_type=AhjoRequestType.GET_DECISION_DETAILS
+            )
