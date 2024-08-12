5af54919f03df7904f0ce64320f3403ed5fd174c
Riku Kestilä <riku.kestila@vincit.fi>
2024-08-12 16:01:43 +0300

    feat: errors from ahjo request to handler's JSON

---
+++ backend/benefit/applications/api/v1/serializers/ahjo_status.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from applications.models import AhjoStatus
+
+
+class AhjoStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AhjoStatus
+        fields = ["modified_at", "error_from_ahjo"]
