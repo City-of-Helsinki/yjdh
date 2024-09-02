eaa1fd17558c92d436c2427364d835192eb3745a
Riku Kestilä <riku.kestila@vincit.fi>
2024-09-02 12:34:18 +0300

    feat: validate/require decisionmaker id, name

--- backend/benefit/applications/api/v1/serializers/decision_text.py
+++ backend/benefit/applications/api/v1/serializers/decision_text.py
@@ -7,3 +7,18 @@ class DecisionTextSerializer(serializers.ModelSerializer):
     class Meta:
         model = AhjoDecisionText
         exclude = ["application"]
+
+    def validate(self, data):
+        errors = {}
+
+        decision_maker_name = data.get("decision_maker_name")
+        if not decision_maker_name:
+            errors["decision_maker_name"] = "This field is required."
+
+        decision_maker_id = data.get("decision_maker_id")
+        if not decision_maker_id:
+            errors["decision_maker_id"] = "This field is required."
+
+        if errors:
+            raise serializers.ValidationError(errors)
+        return data
