da09d5976ed0b038e64b83dcd41da08c92b16c72
Mikko Riippi <mikko.riippi@gofore.com>
2024-11-06 14:37:53 +0200

    chore: update tet-admin

--- frontend/tet/admin/src/components/header/Header.tsx
+++ frontend/tet/admin/src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import React from 'react';
-import BaseHeader from 'shared/components/header/Header';
+import BaseHeader from 'shared/components/header/HeaderV3';
 import useGoToFrontPage from 'shared/hooks/useGoToFrontPage';
 import { SUPPORTED_LANGUAGES } from 'shared/i18n/i18n';
 import { OptionType } from 'shared/types/common';
@@ -25,8 +25,7 @@ const Header: React.FC = () => {
   );
 
   const handleLanguageChange = React.useCallback(
-    (e: React.SyntheticEvent<unknown>, { value: lang }: OptionType<string>): void => {
-      e.preventDefault();
+    (lang: OptionType<string>): void => {
       void router.push(asPath, asPath, {
         locale: lang,
       });
