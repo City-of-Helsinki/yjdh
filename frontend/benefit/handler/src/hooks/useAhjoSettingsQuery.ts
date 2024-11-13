e595189d0ab04c895f1f56fd96ed071dcd178402
Riku Kestilä <riku.kestila@vincit.fi>
2024-11-13 12:19:52 +0200

    feat: ahjo signer frontend radio inputs

--- frontend/benefit/handler/src/hooks/useAhjoSettingsQuery.ts
+++ frontend/benefit/handler/src/hooks/useAhjoSettingsQuery.ts
@@ -1,6 +1,8 @@
 import { BackendEndpoint } from 'benefit-shared/backend-api/backend-api';
 import {
   AhjoSettingsResponse,
+  AhjoSigner,
+  AhjoSignerOptions,
   DecisionMaker,
   DecisionMakerOptions,
 } from 'benefit-shared/types/application';
@@ -8,19 +10,28 @@ import camelcaseKeys from 'camelcase-keys';
 import { useQuery, UseQueryResult } from 'react-query';
 import useBackendAPI from 'shared/hooks/useBackendAPI';
 
-const useAhjoSettingsQuery = (): UseQueryResult<
-  DecisionMakerOptions,
+type AhjoSettingsType = 'ahjo_decision_maker' | 'ahjo_signer';
+
+const useAhjoSettingsQuery = (type: AhjoSettingsType): UseQueryResult<
+  DecisionMakerOptions | AhjoSignerOptions,
   Error
 > => {
   const { axios, handleResponse } = useBackendAPI();
-  return useQuery<DecisionMakerOptions, Error>(['decisionMakerOptions'], () =>
+  return useQuery<DecisionMakerOptions | AhjoSignerOptions, Error>(['ahjoSettings', type], () =>
     handleResponse<AhjoSettingsResponse>(
-      axios.get(`${String(BackendEndpoint.AHJO_SETTINGS)}`)
+      axios.get(`${String(BackendEndpoint.AHJO_SETTINGS)}${type}`)
     ).then(
-      (response) =>
-        response?.data?.map((item: DecisionMaker) =>
-          camelcaseKeys(item, { deep: true })
-        ) || []
+      (response) => {
+        if (type === 'ahjo_decision_maker') {
+          return (response?.data || []).map((item: DecisionMaker) =>
+            camelcaseKeys(item, { deep: true })
+          );
+        } 
+          return (response?.data || []).map((item: AhjoSigner) =>
+            camelcaseKeys(item, { deep: true })
+          );
+        
+      }
     )
   );
 };
