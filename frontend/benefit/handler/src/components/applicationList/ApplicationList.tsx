42b7751aa5eb97fa47003b6ce5d519c9c7a4f9d0
Sampo Tawast <sampo.tawast@gmail.com>
2024-12-05 15:25:19 +0200

    fix: try to parse all kinds of ahjo errors

--- frontend/benefit/handler/src/components/applicationList/ApplicationList.tsx
+++ frontend/benefit/handler/src/components/applicationList/ApplicationList.tsx
@@ -109,6 +109,19 @@ export const renderPaymentTagPerStatus = (
   </$TagWrapper>
 );
 
+const renderAhjoError = (ahjoError: AhjoError): JSX.Element[] => {
+  if (Array.isArray(ahjoError.errorFromAhjo))
+    return ahjoError.errorFromAhjo.map(({ message }) => <li>{message}</li>);
+
+  if (
+    typeof ahjoError.errorFromAhjo === 'object' &&
+    ahjoError.errorFromAhjo.message
+  )
+    return [<li>{ahjoError.errorFromAhjo.message}</li>];
+
+  return [<li>{String(ahjoError.errorFromAhjo)}</li>];
+};
+
 const ApplicationList: React.FC<ApplicationListProps> = ({
   heading,
   status,
@@ -177,13 +190,7 @@ const ApplicationList: React.FC<ApplicationListProps> = ({
                   Ahjo, {convertToUIDateAndTimeFormat(ahjoError?.modifiedAt)}
                 </strong>
               </div>
-              <ul>
-                {ahjoError?.errorFromAhjo?.map
-                  ? ahjoError?.errorFromAhjo?.map(({ message }) => (
-                      <li>{message}</li>
-                    ))
-                  : ahjoError?.errorFromAhjo}
-              </ul>
+              <ul>{renderAhjoError(ahjoError)}</ul>
             </Tooltip>
           </$ActionErrors>
         )}
