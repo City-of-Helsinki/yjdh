8d91282a1763054bb2c4ce55b8cb4d65da0b9168
Mikko Riippi <mikko.riippi@gofore.com>
2024-12-12 12:49:50 +0200

    fix: typos and array

--- frontend/kesaseteli/youth/src/components/header/__tests__/Header.test.tsx
+++ frontend/kesaseteli/youth/src/components/header/__tests__/Header.test.tsx
@@ -3,15 +3,16 @@ import headerApi from 'kesaseteli-shared/__tests__/utils/component-apis/header-a
 import renderComponent from 'kesaseteli-shared/__tests__/utils/components/render-component';
 import React from 'react';
 import { waitFor } from 'shared/__tests__/utils/test-utils';
+import { SUPPORTED_LANGUAGES } from 'shared/i18n/i18n';
 
-// Languages need to start with sometning else than 'fi' to avoid the default language
-const LANGUAGES = ['sv', 'en', 'fi'] as const;
+// Languages need to start with something else than 'fi' to avoid the default language
+const REVERSED_LANGUAGES = [...SUPPORTED_LANGUAGES].reverse();
 
 describe('frontend/kesaseteli/youth/src/components/header/Header.tsx', () => {
   it('can change supported language', async () => {
     const spyRouterPush = jest.fn();
     renderComponent(<Header />, { push: spyRouterPush });
-    for (const lang of LANGUAGES) {
+    for (const lang of REVERSED_LANGUAGES) {
       await headerApi.actions.changeLanguage(lang);
       await waitFor(() =>
         expect(spyRouterPush).toHaveBeenCalledWith(undefined, undefined, {
