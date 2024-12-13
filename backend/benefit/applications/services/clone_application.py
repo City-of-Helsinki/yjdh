cce4714c05418061ca938d2956f33cacd6bcd29b
JanneJuhola <janne.juhola@varaani.com>
2024-12-13 13:57:57 +0200

    fix: In application cloning force origin to be applicant

--- backend/benefit/applications/services/clone_application.py
+++ backend/benefit/applications/services/clone_application.py
@@ -8,6 +8,7 @@ from applications.enums import (
     ApplicationStatus,
     ApplicationStep,
     AttachmentType,
+    ApplicationOrigin
 )
 from applications.models import (
     AhjoStatus,
@@ -42,7 +43,7 @@ def clone_application_based_on_other(
             "alternative_company_postcode": application_base.alternative_company_postcode,
             "alternative_company_street_address": application_base.alternative_company_street_address,
             "applicant_language": "fi",
-            "application_origin": application_base.application_origin,
+            "application_origin": ApplicationOrigin.applicant,
             "application_step": ApplicationStep.STEP_1,
             "archived": False,
             "association_has_business_activities": application_base.association_has_business_activities
