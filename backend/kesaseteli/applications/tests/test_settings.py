7e8f3eef6c3ffa1c3723690f1b95c326d8060bdd
Kari Salminen <kari.salminen@anders.com>
2025-09-16 14:40:25 +0300

    test(kesaseteli): add xfailing tests for upcoming Browsable API removal
    The added tests expectedly fail currently as Django Rest Framework's
    Browsable API is enabled.
    
    refs YJDH-739

---
+++ backend/kesaseteli/applications/tests/test_settings.py
@@ -0,0 +1,16 @@
+import pytest
+from rest_framework.renderers import BrowsableAPIRenderer
+from rest_framework.settings import api_settings
+
+
+@pytest.mark.xfail(reason="Browsable API is not yet disabled, but will be")
+def test_django_rest_framework_browsable_api_disabled():
+    """
+    Test that Django Rest Framework's Browsable API is disabled.
+
+    This is done to make sure that no information is leaked through
+    the Browsable API interface as it can populate dropdown menus with
+    information from the backend if it is enabled.
+    """
+    for renderer_class in api_settings.DEFAULT_RENDERER_CLASSES:
+        assert not issubclass(renderer_class, BrowsableAPIRenderer)
