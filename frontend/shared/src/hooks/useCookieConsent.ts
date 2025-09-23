4cdcd3acd757d3a68cbf6405fe77d1a3126b756b
mikkojamG <mikko.jamia@gofore.com>
2025-09-23 08:19:07 +0300

    fix(benefit): matomo lint fixes Ref: HL-1604

--- frontend/shared/src/hooks/useCookieConsent.ts
+++ frontend/shared/src/hooks/useCookieConsent.ts
@@ -10,30 +10,37 @@ type UseCookieConsentHook = () => {
   optionalCookies: OptionalCookies;
 };
 
-const useCookieConsent: UseCookieConsentHook = () => {
-  const onAllConsentsGiven = (consents: { matomo: boolean }): void => {
-    if (MATOMO_ENABLED === 'true' && consents.matomo) {
-      //  start tracking
-      window._paq.push(['setConsentGiven']);
-      window._paq.push(['setCookieConsentGiven']);
-    }
-  };
+const onAllConsentsGiven = (consents: { matomo: boolean }): void => {
+  if (MATOMO_ENABLED === 'true' && consents.matomo) {
+    //  start tracking
+    // eslint-disable-next-line no-underscore-dangle
+    window._paq.push(['setConsentGiven']);
+    // eslint-disable-next-line no-underscore-dangle, unicorn/no-array-push-push
+    window._paq.push(['setCookieConsentGiven']);
+  }
+};
 
-  const onConsentsParsed = (consents: { matomo: boolean }): void => {
-    if (MATOMO_ENABLED === 'true') {
-      if (consents.matomo === undefined) {
-        // tell matomo to wait for consent:
-        window._paq.push(['requireConsent']);
-        window._paq.push(['requireCookieConsent']);
-      } else if (consents.matomo === false) {
-        // tell matomo to forget conset
-        if (window && window._paq) {
-          window._paq.push(['forgetConsentGiven']);
-        }
+const onConsentsParsed = (consents: { matomo: boolean }): void => {
+  if (MATOMO_ENABLED === 'true') {
+    if (consents.matomo === undefined) {
+      // tell matomo to wait for consent:
+      // eslint-disable-next-line no-underscore-dangle
+      window._paq.push(['requireConsent']);
+      // eslint-disable-next-line no-underscore-dangle, unicorn/no-array-push-push
+      window._paq.push(['requireCookieConsent']);
+      // eslint-disable-next-line sonarjs/no-collapsible-if
+    } else if (consents.matomo === false) {
+      // tell matomo to forget conset
+      // eslint-disable-next-line no-underscore-dangle, unicorn/no-lonely-if
+      if (window && window._paq) {
+        // eslint-disable-next-line no-underscore-dangle
+        window._paq.push(['forgetConsentGiven']);
       }
     }
-  };
+  }
+};
 
+const useCookieConsent: UseCookieConsentHook = () => {
   const optionalCookies: OptionalCookies = {
     groups: [
       {
