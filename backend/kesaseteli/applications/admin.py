135b29ea93a6672c2763975a52a01d639c7e565d
Niko Lindroos <niko.j.makela@gmail.com>
2026-01-08 12:54:06 +0200

    feat(kesaseteli,admin): more features in school admin page
    YJDH-694.

--- backend/kesaseteli/applications/admin.py
+++ backend/kesaseteli/applications/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 
 from applications.models import School
 
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "created_at",
+        "modified_at",
+    ]
+    list_filter = [
+        "created_at",
+        "modified_at",
+    ]
+    date_hierarchy = "created_at"
+    search_fields = ["name"]
+
+
 if apps.is_installed("django.contrib.admin"):
-    admin.site.register(School)
+    admin.site.register(School, SchoolAdmin)
