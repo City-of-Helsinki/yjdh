95abb06abb2712e4461f977a2e4efd972beb0609
ext-hjasplund <henrik.asplund@leasedealit.fi>
2026-01-14 09:21:45 +0200

    fix: ui changes for 2026 rules
    UI changed to reflect the 2026 changes in application.
    Changed validation logic.
    Changed localization strings.
    Changed form submit data to contain missing fields
    Removing paySubsidyPercent from submit request.
    
    Refs: HL-1706

--- frontend/benefit/applicant/src/components/applications/forms/application/step6/useApplicationFormStep6.ts
+++ frontend/benefit/applicant/src/components/applications/forms/application/step6/useApplicationFormStep6.ts
@@ -95,7 +95,6 @@ const useApplicationFormStep6 = (
           },
           apprenticeshipProgram: application?.apprenticeshipProgram,
           paySubsidyGranted: PAY_SUBSIDY_GRANTED.NOT_GRANTED,
-          paySubsidyPercent: null,
           trainingCompensations: application?.apprenticeshipProgram
             ? application?.trainingCompensations
             : [],
