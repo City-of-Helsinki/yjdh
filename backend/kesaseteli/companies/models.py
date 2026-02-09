9a7981ac217e61fe57874cada33e76b18dd5f223
Kari Salminen <kari.salminen@anders.com>
2026-02-09 16:20:42 +0200

    feat(ks,backend): index Company.name for search & ordering performance
    refs YJDH-805

--- backend/kesaseteli/companies/models.py
+++ backend/kesaseteli/companies/models.py
@@ -40,6 +40,9 @@ class Company(UUIDModel, TimeStampedModel):
         verbose_name = _("company")
         verbose_name_plural = _("companies")
         ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"], name="company_name_idx"),
+        ]
 
     def __str__(self):
         return f"{self.name} ({self.business_id})"
