2094a077c86b575721cf106258494fededcbaad7
Kari Salminen <kari.salminen@anders.com>
2026-02-23 17:34:29 +0200

    fix(frontend,all): fix use of env vars in /frontend/next.config.js
    TL;DR:
    ```
    In /frontend/next.config.js:
    1. Move env var readings inside nextConfig function so env vars set
       using .env.* files are read only after setting them (e.g. the sentry
       related variables)
    2. Handle overriding env vars gracefully
    3. Remove unnecessary withPlugins / next-compose-plugins
    ```
    
    Done:
    - Move all readings of env vars from module level to inside the
      nextConfig function so that the environment variables set in the
      applications' next.config.js files using dotenv are in use when the
      env vars' values are evaluated.
    - Destructure `env` from nextConfig's input and spread it into `env` key
      in nextConfig function's return value, so the `env` override is
      combined with the APP_NAME, APP_VERSION and BUILD_TIME settings rather
      than removing/overriding them directly as was the case with the
      `override` parameter before. Spread the rest of the nextConfigs input
      as previously.
    - Remove withPlugins and next-compose-plugins as unnecessary as the used
      plugins were always empty. The next-compose-plugins package was last
      published on npm 5 years ago, so all the more reason to get rid of it.
    
    refs YJDH-813

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const withPlugins = require('next-compose-plugins');
 const { withSentryConfig } = require('@sentry/nextjs');
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
@@ -7,14 +6,6 @@ const pc = require('picocolors');
 const packageJson = require('./package.json');
 
 const trueEnv = ['true', '1', 'yes'];
-
-const NEXTJS_IGNORE_ESLINT = trueEnv.includes(process.env?.NEXTJS_IGNORE_ESLINT ?? 'false');
-const NEXTJS_IGNORE_TYPECHECK = trueEnv.includes(process.env?.NEXTJS_IGNORE_TYPECHECK ?? 'false');
-const NEXTJS_DISABLE_SENTRY = trueEnv.includes(process.env?.NEXTJS_DISABLE_SENTRY ?? 'false');
-const NEXTJS_SENTRY_UPLOAD_DRY_RUN = trueEnv.includes(process.env?.NEXTJS_SENTRY_UPLOAD_DRY_RUN ?? 'false');
-const NEXTJS_SENTRY_DEBUG = trueEnv.includes(process.env?.NEXTJS_SENTRY_DEBUG ?? 'false');
-const NEXTJS_SENTRY_TRACING = trueEnv.includes(process.env?.NEXTJS_SENTRY_TRACING ?? 'false');
-const WATCHPACK_POLLING = trueEnv.includes(process.env?.WATCHPACK_POLLING ?? 'false');
 
 // Get the app context ...
 let appName;
@@ -26,148 +17,155 @@ if (process.cwd().indexOf('/app/') === 0) {
   appName = process.cwd().split('/frontend/').pop();
 }
 
-/**
- * A way to allow CI optimization when the build done there is not used
- * to deliver an image or deploy the files.
- * @link https://nextjs.org/docs/advanced-features/source-maps
- */
-const disableSourceMaps = trueEnv.includes(process.env?.NEXT_DISABLE_SOURCEMAPS ?? 'false');
+const nextConfig = ({ env: envOverrides, ...restOverrides }) => {
+  const NEXTJS_IGNORE_ESLINT = trueEnv.includes(process.env?.NEXTJS_IGNORE_ESLINT ?? 'false');
+  const NEXTJS_IGNORE_TYPECHECK = trueEnv.includes(process.env?.NEXTJS_IGNORE_TYPECHECK ?? 'false');
+  const NEXTJS_DISABLE_SENTRY = trueEnv.includes(process.env?.NEXTJS_DISABLE_SENTRY ?? 'false');
+  const NEXTJS_SENTRY_UPLOAD_DRY_RUN = trueEnv.includes(process.env?.NEXTJS_SENTRY_UPLOAD_DRY_RUN ?? 'false');
+  const NEXTJS_SENTRY_DEBUG = trueEnv.includes(process.env?.NEXTJS_SENTRY_DEBUG ?? 'false');
+  const NEXTJS_SENTRY_TRACING = trueEnv.includes(process.env?.NEXTJS_SENTRY_TRACING ?? 'false');
+  const WATCHPACK_POLLING = trueEnv.includes(process.env?.WATCHPACK_POLLING ?? 'false');
 
-if (disableSourceMaps) {
-  console.warn(`${pc.yellow('notice')}- Sourcemaps generation have been disabled through NEXT_DISABLE_SOURCEMAPS`);
-}
+  // See https://nextjs.org/docs/advanced-features/source-maps
+  const disableSourceMaps = trueEnv.includes(process.env?.NEXT_DISABLE_SOURCEMAPS ?? 'false');
 
-if (NEXTJS_SENTRY_DEBUG) {
-  console.warn(`${pc.yellow('notice')}- Build won't use sentry treeshaking (NEXTJS_SENTRY_DEBUG)`);
-}
+  if (disableSourceMaps) {
+    console.warn(`${pc.yellow('notice')}- Sourcemaps generation have been disabled through NEXT_DISABLE_SOURCEMAPS`);
+  }
 
-const nextConfig = (override) => ({
-  productionBrowserSourceMaps: !disableSourceMaps,
-  poweredByHeader: false,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
-  experimental: {
-    externalDir: true,
-  },
-  output: 'standalone',
-  typescript: {
-    /** Do not run TypeScript during production builds (`next build`). */
-    ignoreBuildErrors: NEXTJS_IGNORE_TYPECHECK,
-  },
-  eslint: {
-    ignoreDuringBuilds: NEXTJS_IGNORE_ESLINT,
-  },
-  transpilePackages: ['@frontend'],
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-    };
+  if (NEXTJS_SENTRY_DEBUG) {
+    console.warn(`${pc.yellow('notice')}- Build won't use sentry treeshaking (NEXTJS_SENTRY_DEBUG)`);
+  }
 
-    config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /\/(__tests__|test)\// }));
-
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/tree-shaking/
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        __SENTRY_DEBUG__: NEXTJS_SENTRY_DEBUG,
-        __SENTRY_TRACING__: NEXTJS_SENTRY_TRACING,
-      }),
-    );
-
-    config.module.rules.push({
-      test: /\.test.tsx$/,
-      loader: 'ignore-loader',
-    });
-
-    if (appName && appName === 'benefit/applicant') {
-      config.module.rules.push({
-        test: /pdfjs-dist\/build\/pdf\.worker\.js$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/chunks/[name].[hash][ext]',
-        },
-      });
-    }
-
-    // Enable file watching using polling if WATCHPACK_POLLING is enabled.
-    // This makes hot reload work in Docker on e.g. Windows using data on
-    // host filesystem. NOTE: Enabling Turbopack makes this not work!
-    if (WATCHPACK_POLLING) {
-      config.watchOptions = {
-        ...config.watchOptions,
-        // https://webpack.js.org/configuration/watch/#watchoptions
-        poll: 1000, // Check for changes every second
-        aggregateTimeout: 300, // Delay 0.3s before rebuilding to group changes
-        ignored: ['**/node_modules', '**/.git', '**/.next'],
+  const config = {
+    productionBrowserSourceMaps: !disableSourceMaps,
+    poweredByHeader: false,
+    swcMinify: true,
+    compiler: {
+      styledComponents: true,
+    },
+    experimental: {
+      externalDir: true,
+    },
+    output: 'standalone',
+    typescript: {
+      /** Do not run TypeScript during production builds (`next build`). */
+      ignoreBuildErrors: NEXTJS_IGNORE_TYPECHECK,
+    },
+    eslint: {
+      ignoreDuringBuilds: NEXTJS_IGNORE_ESLINT,
+    },
+    transpilePackages: ['@frontend'],
+    webpack: (config) => {
+      config.resolve.fallback = {
+        fs: false,
       };
-      console.info('Polling enabled for webpack file watching');
-    } else {
-      console.info('Using native file watching for webpack');
-    }
 
-    return config;
-  },
-  env: {
-    APP_NAME: packageJson.name,
-    APP_VERSION: packageJson.version,
-    BUILD_TIME: new Date().toISOString(),
-  },
-  serverRuntimeConfig: {
-    // to bypass https://github.com/zeit/next.js/issues/8251
-    PROJECT_ROOT: __dirname,
-  },
-  ...override,
-});
+      config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /\/(__tests__|test)\// }));
 
-let config = nextConfig;
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/tree-shaking/
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          __SENTRY_DEBUG__: NEXTJS_SENTRY_DEBUG,
+          __SENTRY_TRACING__: NEXTJS_SENTRY_TRACING,
+        }),
+      );
 
-const cloudSentryEnabled =
-  process.env?.NEXT_PUBLIC_SENTRY_ENVIRONMENT &&
-  process.env?.NEXT_PUBLIC_SENTRY_DSN &&
-  process.env?.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE &&
-  process.env?.NEXT_PUBLIC_SENTRY_TRACE_PROPAGATION_TARGETS &&
-  (process.env?.NEXT_PUBLIC_SENTRY_REPLAYS_SESSION_SAMPLE_RATE !== undefined) &&
-  (process.env?.NEXT_PUBLIC_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE !== undefined)
+      config.module.rules.push({
+        test: /\.test.tsx$/,
+        loader: 'ignore-loader',
+      });
 
-if (cloudSentryEnabled) {
-  console.warn(`${pc.yellow('notice')}- Cloud Sentry variables detected:`, {
-    SENTRY_ENVIRONMENT: process.env?.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
-    SENTRY_DSN: process.env?.NEXT_PUBLIC_SENTRY_DSN ? 'SET' : 'NOT SET',
-    SENTRY_TRACES_SAMPLE_RATE: process.env?.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE,
-    SENTRY_TRACE_PROPAGATION_TARGETS: process.env?.NEXT_PUBLIC_SENTRY_TRACE_PROPAGATION_TARGETS,
-    SENTRY_REPLAYS_SESSION_SAMPLE_RATE: process.env?.NEXT_PUBLIC_SENTRY_REPLAYS_SESSION_SAMPLE_RATE,
-    SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE: process.env?.NEXT_PUBLIC_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE,
-  });
-}
+      if (appName && appName === 'benefit/applicant') {
+        config.module.rules.push({
+          test: /pdfjs-dist\/build\/pdf\.worker\.js$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/chunks/[name].[hash][ext]',
+          },
+        });
+      }
 
-if (!NEXTJS_DISABLE_SENTRY) {
-  console.warn(`${pc.yellow('notice')}- Sentry is enabled (NEXTJS_DISABLE_SENTRY)`);
-  // @ts-ignore because sentry does not match nextjs current definitions
-  config = withSentryConfig(config, {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-    ...(cloudSentryEnabled ? {
-      // Only print logs for uploading source maps in CI
-      silent: !process.env.CI,
-      // Disable sourcemap uploading to Sentry
-      sourcemaps: {
-        disable: true,
-      },
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
-      reactComponentAnnotation: {
-        enabled: true,
-      },
-    } : {
-      dryRun: NEXTJS_SENTRY_UPLOAD_DRY_RUN,
-      disableLogger: !NEXTJS_SENTRY_DEBUG,
-    }),
-  });
-} else {
-  console.warn(`${pc.yellow('notice')}- Sentry is disabled (NEXTJS_DISABLE_SENTRY)`);
-}
+      // Enable file watching using polling if WATCHPACK_POLLING is enabled.
+      // This makes hot reload work in Docker on e.g. Windows using data on
+      // host filesystem. NOTE: Enabling Turbopack makes this not work!
+      if (WATCHPACK_POLLING) {
+        config.watchOptions = {
+          ...config.watchOptions,
+          // https://webpack.js.org/configuration/watch/#watchoptions
+          poll: 1000, // Check for changes every second
+          aggregateTimeout: 300, // Delay 0.3s before rebuilding to group changes
+          ignored: ['**/node_modules', '**/.git', '**/.next'],
+        };
+        console.info('Polling enabled for webpack file watching');
+      } else {
+        console.info('Using native file watching for webpack');
+      }
 
-const plugins = [];
+      return config;
+    },
+    env: {
+      APP_NAME: packageJson.name,
+      APP_VERSION: packageJson.version,
+      BUILD_TIME: new Date().toISOString(),
+      ...envOverrides,
+    },
+    serverRuntimeConfig: {
+      // to bypass https://github.com/zeit/next.js/issues/8251
+      PROJECT_ROOT: __dirname,
+    },
+    ...restOverrides,
+  };
 
-module.exports = async (phase) => withPlugins(plugins, config(phase))(phase, { undefined });
+  const cloudSentryEnabled =
+    process.env?.NEXT_PUBLIC_SENTRY_ENVIRONMENT &&
+    process.env?.NEXT_PUBLIC_SENTRY_DSN &&
+    process.env?.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE &&
+    process.env?.NEXT_PUBLIC_SENTRY_TRACE_PROPAGATION_TARGETS &&
+    process.env?.NEXT_PUBLIC_SENTRY_REPLAYS_SESSION_SAMPLE_RATE !== undefined &&
+    process.env?.NEXT_PUBLIC_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE !== undefined;
+
+  if (cloudSentryEnabled) {
+    console.warn(`${pc.yellow('notice')}- Cloud Sentry variables detected:`, {
+      SENTRY_ENVIRONMENT: process.env?.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
+      SENTRY_DSN: process.env?.NEXT_PUBLIC_SENTRY_DSN ? 'SET' : 'NOT SET',
+      SENTRY_TRACES_SAMPLE_RATE: process.env?.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE,
+      SENTRY_TRACE_PROPAGATION_TARGETS: process.env?.NEXT_PUBLIC_SENTRY_TRACE_PROPAGATION_TARGETS,
+      SENTRY_REPLAYS_SESSION_SAMPLE_RATE: process.env?.NEXT_PUBLIC_SENTRY_REPLAYS_SESSION_SAMPLE_RATE,
+      SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE: process.env?.NEXT_PUBLIC_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE,
+    });
+  }
+
+  if (!NEXTJS_DISABLE_SENTRY) {
+    console.warn(`${pc.yellow('notice')}- Sentry is enabled (NEXTJS_DISABLE_SENTRY)`);
+    // @ts-ignore because sentry does not match nextjs current definitions
+    return withSentryConfig(config, {
+      // For all available options, see:
+      // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+      ...(cloudSentryEnabled
+        ? {
+            // Only print logs for uploading source maps in CI
+            silent: !process.env.CI,
+            // Disable sourcemap uploading to Sentry
+            sourcemaps: {
+              disable: true,
+            },
+            // Automatically tree-shake Sentry logger statements to reduce bundle size
+            disableLogger: true,
+            reactComponentAnnotation: {
+              enabled: true,
+            },
+          }
+        : {
+            dryRun: NEXTJS_SENTRY_UPLOAD_DRY_RUN,
+            disableLogger: !NEXTJS_SENTRY_DEBUG,
+          }),
+    });
+  } else {
+    console.warn(`${pc.yellow('notice')}- Sentry is disabled (NEXTJS_DISABLE_SENTRY)`);
+  }
+
+  return config;
+};
+
+module.exports = nextConfig;
