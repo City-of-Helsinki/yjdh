dec19798c171cc6bcfbe3866f24d74525c9f1692
Niko Lindroos <niko.j.makela@gmail.com>
2026-02-25 16:28:07 +0200

    fix(ks,shared): update axios for consistency and fix Nock test setup
    YJDH-801.
    
    Multiple version of Axios were installed in monorepo and Nock tool to
    mock responses for requests done by Axios was failing, when Kesäseteli
    Employer UI's unit tests were run. This commit fixes both issues.

--- frontend/kesaseteli/employer/browser-tests/types.ts
+++ frontend/kesaseteli/employer/browser-tests/types.ts
@@ -13,3 +13,30 @@ export type MockResponse = {
   statusCode: number;
   setBody: (body: string | object | Buffer) => void;
 };
+
+export type HeadersInput =
+  | Record<string, unknown>
+  | { toJSON(): Record<string, unknown> };
+
+export const isAxiosHeaders = (
+  headers: HeadersInput
+): headers is { toJSON(): Record<string, unknown> } =>
+  Boolean(
+    headers &&
+    typeof headers === 'object' &&
+    'toJSON' in headers &&
+    typeof (headers as { toJSON: () => Record<string, unknown> }).toJSON ===
+    'function'
+  );
+
+export type VoucherData = {
+  id: string;
+  summer_voucher_serial_number?: string;
+  target_group?: string;
+  employee_name?: string;
+  employee_ssn: string;
+  employee_phone_number: string;
+  employee_home_city: string;
+  employee_postcode: string;
+  employee_school: string;
+};
--- frontend/shared/src/__tests__/utils/create-axios-test-context.ts
+++ frontend/shared/src/__tests__/utils/create-axios-test-context.ts
@@ -1,12 +1,23 @@
 import Axios, { AxiosInstance } from 'axios';
 
-const createAxiosTestContext = (baseURL: string): AxiosInstance =>
-  Axios.create({
+const createAxiosTestContext = (baseURL: string): AxiosInstance => {
+  const axios = Axios.create({
     baseURL,
     headers: {
       'Content-Type': 'application/json',
     },
     withCredentials: false,
   });
+
+  // To use Nock with Axios, you may need to configure Axios to use the Node adapter
+  // More info https://github.com/nock/nock#axios
+  //
+  // If you are using jsdom, axios will default to using the XHR adapter which
+  // can't be intercepted by nock. So, configure axios to use the node adapter.
+  // References:
+  // https://github.com/axios/axios/pull/5277
+  axios.defaults.adapter = 'http';
+  return axios;
+};
 
 export default createAxiosTestContext;
