38954810042c32955d2abbee804da3d5ee9cdac6
Niko Lindroos <niko.j.makela@gmail.com>
2026-02-27 11:17:23 +0200

    feat(ks,api): employer can delete draft application
    YJDH-816.
    
    If a application filling process is cancelled, also the created draft
    should be deleted.

--- backend/kesaseteli/applications/tests/test_security_with_non_staff_user.py
+++ backend/kesaseteli/applications/tests/test_security_with_non_staff_user.py
@@ -776,7 +776,12 @@ def test_employer_application_detail_unallowed_methods(
     application = attachment.summer_voucher.application
     url = reverse("v1:employerapplication-detail", kwargs={"pk": application.id})
     set_company_business_id_to_client(company, user_client)
-    assert user_client.delete(url).status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+    if application_status == EmployerApplicationStatus.DRAFT:
+        assert user_client.delete(url).status_code == status.HTTP_204_NO_CONTENT
+    elif application_status == EmployerApplicationStatus.SUBMITTED:
+        assert user_client.delete(url).status_code == status.HTTP_400_BAD_REQUEST
+    else:
+        assert user_client.delete(url).status_code == status.HTTP_404_NOT_FOUND
     assert user_client.post(url).status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
 
