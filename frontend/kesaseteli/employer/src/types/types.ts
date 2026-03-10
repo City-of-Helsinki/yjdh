b0b6002e54c02d90ae7fbff3fa51b4de17215fb4
Niko Lindroos <niko.j.makela@gmail.com>
2026-03-10 17:23:05 +0200

    style(ks,employer): fix tab sizes
    YJDH-816.

--- frontend/kesaseteli/employer/src/types/types.ts
+++ frontend/kesaseteli/employer/src/types/types.ts
@@ -2,11 +2,11 @@ import Application from 'shared/types/application';
 import Employment from 'shared/types/employment';
 
 export type DashboardVoucher = Employment & {
-    applicationId: string;
-    applicationStatus: Application['status'];
-    modified_at: string;
+  applicationId: string;
+  applicationStatus: Application['status'];
+  modified_at: string;
 };
 
 export type DashboardApplication = Application & {
-    modified_at: string;
+  modified_at: string;
 };
