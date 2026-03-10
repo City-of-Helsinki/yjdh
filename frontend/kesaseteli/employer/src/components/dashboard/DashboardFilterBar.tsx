26ac6a442ac645f5df03c86b08a969338f1caf66
Niko Lindroos <niko.j.makela@gmail.com>
2026-03-10 17:23:05 +0200

    style(ks,employer): fix tab sizes
    YJDH-816.

--- frontend/kesaseteli/employer/src/components/dashboard/DashboardFilterBar.tsx
+++ frontend/kesaseteli/employer/src/components/dashboard/DashboardFilterBar.tsx
@@ -12,28 +12,28 @@ const $FilterBar = styled.div`
 `;
 
 type Props = {
-    showOnlyMine: boolean;
-    onToggle: () => void;
+  showOnlyMine: boolean;
+  onToggle: () => void;
 };
 
 const DashboardFilterBar: React.FC<Props> = ({ showOnlyMine, onToggle }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <$FilterBar>
-            <ToggleButton
-                id="application-table-filter-mine"
-                label={t('common:dashboard.filterBar.label')}
-                aria-label={t('common:dashboard.filterBar.ariaLabel')}
-                variant="inline"
-                checked={showOnlyMine}
-                onChange={onToggle}
-                tooltipLabel={t('common:dashboard.filterBar.tooltipLabel')}
-                tooltipText={t('common:dashboard.filterBar.tooltipText')}
-                tooltipButtonLabel={t('common:dashboard.filterBar.tooltipButtonLabel')}
-            />
-        </$FilterBar>
-    );
+  return (
+    <$FilterBar>
+      <ToggleButton
+        id="application-table-filter-mine"
+        label={t('common:dashboard.filterBar.label')}
+        aria-label={t('common:dashboard.filterBar.ariaLabel')}
+        variant="inline"
+        checked={showOnlyMine}
+        onChange={onToggle}
+        tooltipLabel={t('common:dashboard.filterBar.tooltipLabel')}
+        tooltipText={t('common:dashboard.filterBar.tooltipText')}
+        tooltipButtonLabel={t('common:dashboard.filterBar.tooltipButtonLabel')}
+      />
+    </$FilterBar>
+  );
 };
 
 export default DashboardFilterBar;
