b38e662a25f70440471123d767881e3631e3fd06
Niko Lindroos <niko.j.makela@gmail.com>
2026-03-11 12:23:44 +0200

    feat(ks,handler): add header with login and logout features
    YJDH-818.
    
    Add (a HDS designed) top navigation bar / header to allow login and
    logout directly from the Handlers UI.

---
+++ frontend/kesaseteli/handler/src/hooks/backend/useUserQuery.ts
@@ -0,0 +1,29 @@
+import { BackendEndpoint } from 'kesaseteli-shared/backend-api/backend-api';
+import {
+  QueryKey,
+  useQuery,
+  UseQueryOptions,
+  UseQueryResult,
+} from 'react-query';
+import useErrorHandler from 'shared/hooks/useErrorHandler';
+import useGoToPage from 'shared/hooks/useGoToPage';
+import useIsRouting from 'shared/hooks/useIsRouting';
+import User from 'shared/types/user';
+
+const useUserQuery = <T = User>({
+  refetchInterval,
+  select,
+  enabled = true,
+}: UseQueryOptions<T> = {}): UseQueryResult<T> => {
+  const isRouting = useIsRouting();
+  const goToPage = useGoToPage();
+  return useQuery(BackendEndpoint.USER as QueryKey, {
+    enabled: !!enabled && !isRouting,
+    onError: useErrorHandler({
+      onServerError: () => goToPage('/login?error=true'),
+    }),
+    select,
+    refetchInterval,
+  });
+};
+export default useUserQuery;
