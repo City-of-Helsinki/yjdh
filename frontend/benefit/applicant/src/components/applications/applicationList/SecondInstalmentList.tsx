381d3e64972c314830fad39ef54c042322dede9c
ext-hjasplund <henrik.asplund@leasedealit.fi>
2026-04-17 09:15:13 +0300

    feat(benefit): 2nd instalment requested to front page
    Add applications with 2nd instalment status
    REQUESTED to front page.
    
    Refs: HL-1745

---
+++ frontend/benefit/applicant/src/components/applications/applicationList/SecondInstalmentList.tsx
@@ -0,0 +1,69 @@
+import ListContents from 'benefit/applicant/components/applications/applicationList/listItem/ListContents';
+import React from 'react';
+import { OptionType } from 'shared/types/common';
+
+import ListItem from './listItem/ListItem';
+import useSecondInstalmentApplicationList from "./useSecondInstalmentApplicationList";
+
+export interface ApplicationListProps {
+  heading: React.ReactNode;
+  status: string[];
+  isArchived?: boolean;
+  orderByOptions?: OptionType[];
+  noItemsText?: React.ReactNode;
+  onListLengthChanged?: (isLoading: boolean, length: number) => void;
+  beforeList?: React.ReactNode;
+  afterList?: React.ReactNode;
+}
+
+const SecondInstalmentList: React.FC<ApplicationListProps> = ({
+                                                           heading,
+                                                           status,
+                                                           isArchived,
+                                                           orderByOptions,
+                                                           secondInstalmentStatus,
+                                                           noItemsText,
+                                                           onListLengthChanged,
+                                                           beforeList,
+                                                           afterList,
+                                                         }) => {
+  const {
+    list,
+    shouldShowSkeleton,
+    shouldHideList,
+    t,
+    orderBy,
+    setOrderBy,
+    hasItems,
+  } = useSecondInstalmentApplicationList({
+    status,
+    isArchived,
+    orderByOptions,
+    secondInstalmentStatus,
+  });
+
+  const items =
+    list?.map((props) => <ListItem key={props.id} {...props} />) || [];
+
+  return (
+    <ListContents
+      list={list}
+      shouldShowSkeleton={shouldShowSkeleton}
+      shouldHideList={shouldHideList}
+      t={t}
+      orderBy={orderBy}
+      setOrderBy={setOrderBy}
+      orderByOptions={orderByOptions}
+      hasItems={hasItems}
+      headingText={heading}
+      status={status}
+      items={items}
+      noItemsText={noItemsText}
+      onListLengthChanged={onListLengthChanged}
+      beforeList={beforeList}
+      afterList={afterList}
+    />
+  );
+};
+
+export default SecondInstalmentList;
