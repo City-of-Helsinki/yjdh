77f7fc52bbc575500479b5482883169deb1dc8c7
Toivo Mattila <toivo.mattila@virnex.fi>
2026-04-21 13:13:12 +0300

    fix(ks,shared): type LinkButton for supplementary variant
    Remove the ts-ignore by typing props against ButtonProps and passing
    ButtonVariant.Supplementary explicitly.
    
    refs: YJDH-824

--- frontend/shared/src/components/link-button/LinkButton.tsx
+++ frontend/shared/src/components/link-button/LinkButton.tsx
@@ -1,9 +1,12 @@
-import { SupplementaryButtonProps } from 'hds-react';
+import { ButtonProps, ButtonVariant } from 'hds-react';
 import React from 'react';
 
 import $LinkButton from './LinkButton.sc';
 
-type Props = Omit<SupplementaryButtonProps, 'size' | 'variant' | 'disabled'>;
+type Props = Omit<ButtonProps, 'size' | 'variant'> & {
+  isLoading?: boolean;
+  loadingText?: string;
+};
 
 /**
  * LinkButton is a button that looks like a link.
@@ -13,18 +16,15 @@ const LinkButton: React.FC<Props> = ({
   isLoading,
   loadingText,
   ...rest
-}: Props) => (
-  // for some reason variant="supplementary" isn't allowed by typescript
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  <$LinkButton
-    variant="supplementary"
-    isLoading={isLoading}
-    loadingText={loadingText}
-    {...rest}
-  >
-    {children}
-  </$LinkButton>
-);
+}: Props) => {
+  const linkButtonProps = {
+    ...rest,
+    isLoading,
+    loadingText,
+    variant: ButtonVariant.Supplementary,
+  } as React.ComponentProps<typeof $LinkButton>;
+
+  return <$LinkButton {...linkButtonProps}>{children}</$LinkButton>;
+};
 
 export default LinkButton;
