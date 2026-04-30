9be23f381a143cce49bf02b22348f6e1dc113f27
Jori Lindell <jori.lindell@gmail.com>
2026-04-30 06:12:34 +0300

    test(frontend): tune test coverage scope
    Refine Jest and Sonar coverage config across frontend apps to focus
    on relevant source files and exclude non-target paths.
    
    - Limit coverage collection to src
    - Exclude src/pages where needed (kesaseteli employer/handler/youth)
    - Exclude style component files (*.sc.ts)
    - Exclude src/types and src/utils/test-utils
    - Reformat Sonar exclusions for readability in kesaseteli projects
    
    Also fix test reliability/lint issues in kesaseteli employer:
    - Make foreign IBAN note assertion more robust in the test helper
    - Add targeted eslint suppression for cert file fs access in
      browser test mocks
    
    Refs: HL-1680

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,7 +5,12 @@ module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!(uuid)/)'],
   setupFiles: [require.resolve('./jest.setup.js')],
   testEnvironment: 'jsdom',
-  collectCoverageFrom: ['./**/*.{ts,tsx,js,jsx}'],
+  collectCoverageFrom: [
+    './src/**/*.{ts,tsx,js,jsx}',
+    '!./src/**/*.sc.ts',
+    '!./src/types/**/*',
+    '!./src/utils/test-utils/**/*',
+  ],
   modulePathIgnorePatterns: ['.next'],
   testPathIgnorePatterns: ['__tests__/utils', '__tests__/types', 'component-apis'],
 };
