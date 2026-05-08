8fc5d93188f47738ed9f956e113919c44c19fd9e
Mikko Riippi <mikko.riippi@gofore.com>
2026-05-08 09:31:07 +0300

    fix: new uuid is not imported properly

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
+  transformIgnorePatterns: ['/node_modules/(?!(uuid)/)'],
+  setupFiles: [require.resolve('./jest.setup.js')],
   testEnvironment: 'jsdom',
   collectCoverageFrom: ['./**/*.{ts,tsx,js,jsx}'],
   modulePathIgnorePatterns: ['.next'],
