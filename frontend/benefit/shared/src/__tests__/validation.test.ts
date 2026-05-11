b32df3ce42f9a2d135ec85d77d3fa0dcf534623d
Jori Lindell <jori.lindell@gmail.com>
2026-05-11 15:00:57 +0300

    feat(benefit,shared): upgrade hds-react to 5.0.0
    Refs: HL-1680

--- frontend/benefit/shared/src/__tests__/validation.test.ts
+++ frontend/benefit/shared/src/__tests__/validation.test.ts
@@ -37,6 +37,7 @@ describe('validateNumberField', () => {
   });
 
   it('should reject undefined (required)', async () => {
+    // eslint-disable-next-line unicorn/no-useless-undefined
     await expect(schema.validate(undefined)).rejects.toThrow();
   });
 });
