bd32ffad73da8a2dde74cc135eeb9b7e5ab944e5
Niko Lindroos <niko.j.makela@gmail.com>
2026-05-18 14:55:06 +0300

    fix(ks): fetch target group translations with current locale
    YJDH-864.

--- frontend/kesaseteli/shared/src/hooks/useSummerVoucherConfigurationQuery.ts
+++ frontend/kesaseteli/shared/src/hooks/useSummerVoucherConfigurationQuery.ts
@@ -1,19 +1,25 @@
 import { BackendEndpoint } from 'kesaseteli-shared/backend-api/backend-api';
 import SummerVoucherConfiguration from 'kesaseteli-shared/types/summer-voucher-configuration';
-import { useTranslation } from 'next-i18next';
 import { useQuery, UseQueryResult } from 'react-query';
 import useBackendAPI from 'shared/hooks/useBackendAPI';
 import useErrorHandler from 'shared/hooks/useErrorHandler';
+import useLocale from 'shared/hooks/useLocale';
 
 const useSummerVoucherConfigurationQuery = (): UseQueryResult<
   SummerVoucherConfiguration[]
 > => {
   const { axios, handleResponse } = useBackendAPI();
-  const { i18n } = useTranslation();
+  const locale = useLocale();
   return useQuery(
-    [BackendEndpoint.SUMMER_VOUCHER_CONFIGURATION, i18n.language],
+    [BackendEndpoint.SUMMER_VOUCHER_CONFIGURATION, locale],
     () =>
-      handleResponse(axios.get(BackendEndpoint.SUMMER_VOUCHER_CONFIGURATION)),
+      handleResponse(
+        axios.get(BackendEndpoint.SUMMER_VOUCHER_CONFIGURATION, {
+          headers: {
+            'Accept-Language': locale,
+          },
+        })
+      ),
     {
       staleTime: Infinity,
       onError: useErrorHandler(),
