a5682a00c6b07c1861e98b26e822f566d9f08113
Niko Lindroos <niko.j.makela@gmail.com>
2026-06-08 16:05:53 +0300

    feat(ks,handler): allow unauthorized access to cookie settings
    YJDH-723.
    
    Exclude cookie-settings route from useUserQuery queries in handler
    Header and prevent auto-redirection to login upon authentication error,
    matching the behavior of the employer UI. Adds a unit test to verify the
    page is accessible without login.

--- frontend/kesaseteli/handler/src/components/header/Header.tsx
+++ frontend/kesaseteli/handler/src/components/header/Header.tsx
@@ -2,14 +2,17 @@ import { IconMover } from 'hds-react';
 import useLogin from 'kesaseteli/handler/hooks/backend/useLogin';
 import useLogout from 'kesaseteli/handler/hooks/backend/useLogout';
 import useUserQuery from 'kesaseteli/handler/hooks/backend/useUserQuery';
+import { ROUTES } from 'kesaseteli-shared/constants/routes';
 import { useTranslation } from 'next-i18next';
 import React from 'react';
 import BaseHeader from 'shared/components/header/Header';
 import useLocale from 'shared/hooks/useLocale';
+import { useRouter } from 'next/router';
 
 const Header: React.FC = () => {
   const { t } = useTranslation();
   const locale = useLocale();
+  const router = useRouter();
 
   const navigationItems = [
     {
@@ -21,7 +24,9 @@ const Header: React.FC = () => {
 
   const login = useLogin();
   const logout = useLogout();
-  const userQuery = useUserQuery();
+  const userQuery = useUserQuery({
+    enabled: !router.asPath?.includes(ROUTES.COOKIE_SETTINGS),
+  });
 
   return (
     <BaseHeader
