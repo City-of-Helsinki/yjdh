2c73e418f71f297c0dfc2ad39a97124124ab9cc5
Jori Lindell <jori.lindell@gmail.com>
2026-06-25 02:18:29 +0300

    chore: remove legacy ts-jest config and dependency
    - remove obsolete ts-jest globals from shared, youth,
      and handler Jest config
    - drop ts-jest from frontend devDependencies
    - refresh pnpm lockfile after test tooling cleanup
    
    Refs: HL-1791

--- frontend/benefit/shared/jest.config.js
+++ frontend/benefit/shared/jest.config.js
@@ -1,11 +1,6 @@
 const sharedConfig = require('../../jest.config.js');
 module.exports = {
   ...sharedConfig,
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/../../tsconfig.json',
-    },
-  },
   moduleNameMapper: {
     ...sharedConfig.moduleNameMapper,
     ['^benefit/shared/test/(.*)$']: '<rootDir>/test/$1',
