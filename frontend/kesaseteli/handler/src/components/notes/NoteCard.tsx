0373537c261587a5e471213b1e9a311bc788d8a0
ext-hjasplund <henrik.asplund@leasedealit.fi>
2026-09-10 09:22:23 +0300

    feat(ks, handler): frontend allows notes to be modified only in 24 hours
    Frontend changes to allow for handler notes to be modified only 24 hours
    after note has been created.
    
    Refs: YJDH-985

--- frontend/kesaseteli/handler/src/components/notes/NoteCard.tsx
+++ frontend/kesaseteli/handler/src/components/notes/NoteCard.tsx
@@ -28,6 +28,11 @@ const NoteCard: React.FC<Props> = ({ note, parentApplicationId }) => {
   const currentUserId = user?.id;
   const isAuthor =
     Boolean(currentUserId) && note.author_username === currentUserId;
+  const createdAtMs = new Date(note.created_at).getTime();
+  const nowMs = Date.now();
+  const isModifiableDate =
+    nowMs >= createdAtMs && nowMs - createdAtMs < 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
   const [isEditing, setIsEditing] = useState(false);
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
 
@@ -82,7 +87,7 @@ const NoteCard: React.FC<Props> = ({ note, parentApplicationId }) => {
       ) : (
         <>
           <$NoteContent>{note.content}</$NoteContent>
-          {isAuthor && (
+          {isAuthor && isModifiableDate && (
             <$NoteActions>
               {/*
                 $ButtonText visually hides the label text on mobile view to prevent layout breaking/wrapping,
